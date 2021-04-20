e09ceafcd1e7d8a28950545f4b7126e4ba2633bb
SimonHe2021 <heyanchun_2000@163.com>
2021-04-20 18:30:30 +0800

    fix(utilityBin2Hex-test.c): fix bug, printf modify to BoatLog, otherwise it will linked error about out function while cross compile.

--- tests/utilityBin2Hex-test.c
+++ tests/utilityBin2Hex-test.c
@@ -20,11 +20,11 @@ int main(int argc, char *argv[])
 	from_str_len = 4;
 
     UtilityBin2Hex(to_str, from_str, from_str_len, BIN2HEX_LEFTTRIM_UNFMTDATA, BIN2HEX_PREFIX_0x_YES, BOAT_FALSE);
-    printf("BIN2HEX_LEFTTRIM_UNFMTDATA = %s\n", to_str);
+    BoatLog(BOAT_LOG_NORMAL,  "BIN2HEX_LEFTTRIM_UNFMTDATA = %s\n", to_str);
 	UtilityBin2Hex(to_str, from_str, from_str_len, BIN2HEX_LEFTTRIM_QUANTITY, BIN2HEX_PREFIX_0x_YES, BOAT_FALSE);
-	printf("BIN2HEX_LEFTTRIM_QUANTITY = %s\n", to_str);
+    BoatLog(BOAT_LOG_NORMAL, "BIN2HEX_LEFTTRIM_QUANTITY = %s\n", to_str);
 	UtilityBin2Hex(to_str, from_str, from_str_len, BIN2HEX_LEFTTRIM_TWOHEXPERBYTE, BIN2HEX_PREFIX_0x_YES, BOAT_FALSE);
-	printf("BIN2HEX_LEFTTRIM_TWOHEXPERBYTE = %s\n", to_str);
+    BoatLog(BOAT_LOG_NORMAL, "BIN2HEX_LEFTTRIM_TWOHEXPERBYTE = %s\n", to_str);
 
 	return 0;
 }
