53e22ebfe10685d5ca483652ada9ab94053ae045
tim-jiang <tim.jiang@aitos.io>
2021-04-30 18:37:17 +0800

    style: remove unused head file

--- include/boatutility.h
+++ include/boatutility.h
@@ -24,7 +24,6 @@ boatutility.h is header file for boatwallet utility functions.
 #define __BOATUTILITY_H__
 
 #include "boatiotsdk.h"
-//#include "boatLog.h"
 
 /*! @defgroup utility boat utility
  * @{
