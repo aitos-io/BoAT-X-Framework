ac05f0244e4b4638927956aa0ee5a096b24e3e50
SimonHe2021 <heyanchun_2000@163.com>
2021-06-08 18:43:46 +0800

    fix(doc/Makefile): modify boatiotsdk to BoAT-X-Framework about N58

--- vendor/platform/Neoway-N58/N58RootDirCode/cmake/toolchain-gcc.cmake
+++ vendor/platform/Neoway-N58/N58RootDirCode/cmake/toolchain-gcc.cmake
@@ -1,4 +1,4 @@
 
-    set(libbw_file_name ${CMAKE_CURRENT_SOURCE_DIR}/boatiotsdk/lib/libboatwallet.a)
-    set(libbh_file_name ${CMAKE_CURRENT_SOURCE_DIR}/boatiotsdk/lib/libboatvendor.a)
+    set(libbw_file_name ${CMAKE_CURRENT_SOURCE_DIR}/BoAT-X-Framework/lib/libboatwallet.a)
+    set(libbh_file_name ${CMAKE_CURRENT_SOURCE_DIR}/BoAT-X-Framework/lib/libboatvendor.a)
 
