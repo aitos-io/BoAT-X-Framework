d9ee75022644071c8ff0f079befd43d5e79ce6c6
Cavalier.liu <1350928576@qq.com>
2021-09-30 10:02:42 +0800

    [chainmaker]add chainmker file and makefile

---
+++ demo/demo_chainmaker/demo_chainmaker.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Copyright (C) 2018-2021 aitos.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+#if defined(__unix__) || defined(__unix) || defined(unix)
+	/* for timestamp source */
+	#define _POSIX_C_SOURCE 200809L
+	#include <time.h>
+#endif
+#include "boatconfig.h"
+#include "boatiotsdk.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
