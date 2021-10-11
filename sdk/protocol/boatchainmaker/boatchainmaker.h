9db969d0a13238e0dc5cf3ef2684698854fa9a5a
Cavalier.liu <1350928576@qq.com>
2021-10-11 14:49:15 +0800

    [chainmaker] add protocol chainamker

---
+++ sdk/protocol/boatchainmaker/boatchainmaker.h
@@ -0,0 +1,39 @@
+/******************************************************************************
+ * Copyright (C) 2018-2021 aitos.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/*!@brief Header file for performing RAW transaction
+
+@file
+boatchainamaker.h is header file for RAW transaction construction and performing.
+*/
+
+#ifndef __BOATCHAINMAKER_H__
+#define __BOATCHAINMAKER_H__
+
+#include "boatconfig.h"
+#include "boatinternal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*! @}*/
+
+#ifdef __cplusplus
+}
+#endif /* end of __cplusplus */
+
+#endif
