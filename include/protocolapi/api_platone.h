0a8cc3b26de9824d8b3ec9ef66a219b1b0c871a9
zzqAlex <44628575+zzqAlex@users.noreply.github.com>
2021-11-22 19:25:13 +0800

    update api_platone.h
    fix the issue #405

--- include/protocolapi/api_platone.h
+++ include/protocolapi/api_platone.h
@@ -437,7 +437,7 @@ __BOATSTATIC __BOATINLINE BOAT_RESULT BoatPlatoneGetTransactionReceipt(BoatPlato
  * @brief Prase RPC method RESPONSE.
  * @see web3_parse_json_result()
  ******************************************************************************/
-BOAT_RESULT BoatPlatonePraseRpcResponseStringResult(const BCHAR *json_string, BoatFieldVariable *result_out)
+BOAT_RESULT BoatPlatonePraseRpcResponseStringResult(const BCHAR *json_string, BoatFieldVariable *result_out);
 
 /*!****************************************************************************
  * @brief Prase RPC method RESPONSE.
