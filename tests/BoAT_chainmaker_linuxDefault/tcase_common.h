830fe770b953cf631298c0eb331358aa4f6c50e3
Cavalier.liu <1350928576@qq.com>
2021-12-03 15:02:35 +0800

    [chainmaker][#436]variable is modified to the macro

--- tests/BoAT_chainmaker_linuxDefault/tcase_common.h
+++ tests/BoAT_chainmaker_linuxDefault/tcase_common.h
@@ -13,50 +13,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *****************************************************************************/
-const BCHAR* chainmaker_user_key =  "-----BEGIN EC PRIVATE KEY-----\n"
-                                    "MHcCAQEEIIs0ji2f0MlhKy6eOPSqknaqzXaL/qErwHyuoYnafy5XoAoGCCqGSM49\n"
-                                    "AwEHoUQDQgAEH4eVrcpsx+G7cCVUHZlTnYQfx1IEfbQxHEsk16MqM/M32cB9fhCo\n"
-                                    "SlqdJUQGYYYLH5hKscO0u0IwFg4THl2bAA==\n"
-                                    "-----END EC PRIVATE KEY-----\n";
+#define     chainmaker_user_key     "-----BEGIN EC PRIVATE KEY-----\n"\
+                                    "MHcCAQEEIIs0ji2f0MlhKy6eOPSqknaqzXaL/qErwHyuoYnafy5XoAoGCCqGSM49\n"\
+                                    "AwEHoUQDQgAEH4eVrcpsx+G7cCVUHZlTnYQfx1IEfbQxHEsk16MqM/M32cB9fhCo\n"\
+                                    "SlqdJUQGYYYLH5hKscO0u0IwFg4THl2bAA==\n"\
+                                    "-----END EC PRIVATE KEY-----\n"
 
 
-const BCHAR* chainmaker_user_cert = "-----BEGIN CERTIFICATE-----\n"
-                                    "MIICaDCCAg6gAwIBAgIDDmQsMAoGCCqGSM49BAMCMIGKMQswCQYDVQQGEwJDTjEQ\n"
-                                    "MA4GA1UECBMHQmVpamluZzEQMA4GA1UEBxMHQmVpamluZzEfMB0GA1UEChMWd3gt\n"
-                                    "b3JnMS5jaGFpbm1ha2VyLm9yZzESMBAGA1UECxMJcm9vdC1jZXJ0MSIwIAYDVQQD\n"
-                                    "ExljYS53eC1vcmcxLmNoYWlubWFrZXIub3JnMB4XDTIxMTEyNDA4NTQzN1oXDTI2\n"
-                                    "MTEyMzA4NTQzN1owgZExCzAJBgNVBAYTAkNOMRAwDgYDVQQIEwdCZWlqaW5nMRAw\n"
-                                    "DgYDVQQHEwdCZWlqaW5nMR8wHQYDVQQKExZ3eC1vcmcxLmNoYWlubWFrZXIub3Jn\n"
-                                    "MQ8wDQYDVQQLEwZjbGllbnQxLDAqBgNVBAMTI2NsaWVudDEuc2lnbi53eC1vcmcx\n"
-                                    "LmNoYWlubWFrZXIub3JnMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEH4eVrcps\n"
-                                    "x+G7cCVUHZlTnYQfx1IEfbQxHEsk16MqM/M32cB9fhCoSlqdJUQGYYYLH5hKscO0\n"
-                                    "u0IwFg4THl2bAKNaMFgwKQYDVR0OBCIEICvG9W6tuYgKgbK5HmSFOPNBcL2m8soF\n"
-                                    "MS1MJjTAgpPvMCsGA1UdIwQkMCKAIPR0GEcka6b/PSqJJ7A3SNvNvNIQkQXhofjh\n"
-                                    "iwjmGTxoMAoGCCqGSM49BAMCA0gAMEUCIQCE8S4SiRCbdvgGpDOXDUICH7WUlXOo\n"
-                                    "N3IHTSI1T2dBRwIgL1CHwXjwM8TtsDA2HPa/U6ad6fe/aHacroEW9OWO3IY=\n"
-                                    "-----END CERTIFICATE-----\n";
+#define     chainmaker_user_cert    "-----BEGIN CERTIFICATE-----\n"\
+                                    "MIICaDCCAg6gAwIBAgIDDmQsMAoGCCqGSM49BAMCMIGKMQswCQYDVQQGEwJDTjEQ\n"\
+                                    "MA4GA1UECBMHQmVpamluZzEQMA4GA1UEBxMHQmVpamluZzEfMB0GA1UEChMWd3gt\n"\
+                                    "b3JnMS5jaGFpbm1ha2VyLm9yZzESMBAGA1UECxMJcm9vdC1jZXJ0MSIwIAYDVQQD\n"\
+                                    "ExljYS53eC1vcmcxLmNoYWlubWFrZXIub3JnMB4XDTIxMTEyNDA4NTQzN1oXDTI2\n"\
+                                    "MTEyMzA4NTQzN1owgZExCzAJBgNVBAYTAkNOMRAwDgYDVQQIEwdCZWlqaW5nMRAw\n"\
+                                    "DgYDVQQHEwdCZWlqaW5nMR8wHQYDVQQKExZ3eC1vcmcxLmNoYWlubWFrZXIub3Jn\n"\
+                                    "MQ8wDQYDVQQLEwZjbGllbnQxLDAqBgNVBAMTI2NsaWVudDEuc2lnbi53eC1vcmcx\n"\
+                                    "LmNoYWlubWFrZXIub3JnMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEH4eVrcps\n"\
+                                    "x+G7cCVUHZlTnYQfx1IEfbQxHEsk16MqM/M32cB9fhCoSlqdJUQGYYYLH5hKscO0\n"\
+                                    "u0IwFg4THl2bAKNaMFgwKQYDVR0OBCIEICvG9W6tuYgKgbK5HmSFOPNBcL2m8soF\n"\
+                                    "MS1MJjTAgpPvMCsGA1UdIwQkMCKAIPR0GEcka6b/PSqJJ7A3SNvNvNIQkQXhofjh\n"\
+                                    "iwjmGTxoMAoGCCqGSM49BAMCA0gAMEUCIQCE8S4SiRCbdvgGpDOXDUICH7WUlXOo\n"\
+                                    "N3IHTSI1T2dBRwIgL1CHwXjwM8TtsDA2HPa/U6ad6fe/aHacroEW9OWO3IY=\n"\
+                                    "-----END CERTIFICATE-----\n"
 
-const BCHAR* chainmaker_tls_ca_cert =   "-----BEGIN CERTIFICATE-----\n"
-                                        "MIICnzCCAkSgAwIBAgIDBbvnMAoGCCqGSM49BAMCMIGKMQswCQYDVQQGEwJDTjEQ\n"
-                                        "MA4GA1UECBMHQmVpamluZzEQMA4GA1UEBxMHQmVpamluZzEfMB0GA1UEChMWd3gt\n"
-                                        "b3JnMS5jaGFpbm1ha2VyLm9yZzESMBAGA1UECxMJcm9vdC1jZXJ0MSIwIAYDVQQD\n"
-                                        "ExljYS53eC1vcmcxLmNoYWlubWFrZXIub3JnMB4XDTIxMTExNzAxNTAxOVoXDTMx\n"
-                                        "MTExNTAxNTAxOVowgYoxCzAJBgNVBAYTAkNOMRAwDgYDVQQIEwdCZWlqaW5nMRAw\n"
-                                        "DgYDVQQHEwdCZWlqaW5nMR8wHQYDVQQKExZ3eC1vcmcxLmNoYWlubWFrZXIub3Jn\n"
-                                        "MRIwEAYDVQQLEwlyb290LWNlcnQxIjAgBgNVBAMTGWNhLnd4LW9yZzEuY2hhaW5t\n"
-                                        "YWtlci5vcmcwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATTrbDKTYGmoROnEIa0\n"
-                                        "YuPQ37OYTChMmE02xS39jillRbkoALddX2V6i35TVvq8SWJO8ewDPEL9RrU2nkqN\n"
-                                        "ALYBo4GWMIGTMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8EBTADAQH/MCkGA1Ud\n"
-                                        "DgQiBCCrEERIXTX0FP05D8KqEHtJk7hwxgl9Gskvuwt/dGvd8TBFBgNVHREEPjA8\n"
-                                        "gg5jaGFpbm1ha2VyLm9yZ4IJbG9jYWxob3N0ghljYS53eC1vcmcxLmNoYWlubWFr\n"
-                                        "ZXIub3JnhwR/AAABMAoGCCqGSM49BAMCA0kAMEYCIQDn8p4qX+LSR8ljZsIGvP2M\n"
-                                        "S4zeal8ndiJb7nBcm+/osQIhAL3MNSBIETeizkfNHE7j1LUWaL83wGtzlfn+BxCN\n"
-                                        "Fqdj\n"
-                                        "-----END CERTIFICATE-----\n";
+#define     chainmaker_tls_ca_cert      "-----BEGIN CERTIFICATE-----\n"\
+                                        "MIICnzCCAkSgAwIBAgIDBbvnMAoGCCqGSM49BAMCMIGKMQswCQYDVQQGEwJDTjEQ\n"\
+                                        "MA4GA1UECBMHQmVpamluZzEQMA4GA1UEBxMHQmVpamluZzEfMB0GA1UEChMWd3gt\n"\
+                                        "b3JnMS5jaGFpbm1ha2VyLm9yZzESMBAGA1UECxMJcm9vdC1jZXJ0MSIwIAYDVQQD\n"\
+                                        "ExljYS53eC1vcmcxLmNoYWlubWFrZXIub3JnMB4XDTIxMTExNzAxNTAxOVoXDTMx\n"\
+                                        "MTExNTAxNTAxOVowgYoxCzAJBgNVBAYTAkNOMRAwDgYDVQQIEwdCZWlqaW5nMRAw\n"\
+                                        "DgYDVQQHEwdCZWlqaW5nMR8wHQYDVQQKExZ3eC1vcmcxLmNoYWlubWFrZXIub3Jn\n"\
+                                        "MRIwEAYDVQQLEwlyb290LWNlcnQxIjAgBgNVBAMTGWNhLnd4LW9yZzEuY2hhaW5t\n"\
+                                        "YWtlci5vcmcwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATTrbDKTYGmoROnEIa0\n"\
+                                        "YuPQ37OYTChMmE02xS39jillRbkoALddX2V6i35TVvq8SWJO8ewDPEL9RrU2nkqN\n"\
+                                        "ALYBo4GWMIGTMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8EBTADAQH/MCkGA1Ud\n"\
+                                        "DgQiBCCrEERIXTX0FP05D8KqEHtJk7hwxgl9Gskvuwt/dGvd8TBFBgNVHREEPjA8\n"\
+                                        "gg5jaGFpbm1ha2VyLm9yZ4IJbG9jYWxob3N0ghljYS53eC1vcmcxLmNoYWlubWFr\n"\
+                                        "ZXIub3JnhwR/AAABMAoGCCqGSM49BAMCA0kAMEYCIQDn8p4qX+LSR8ljZsIGvP2M\n"\
+                                        "S4zeal8ndiJb7nBcm+/osQIhAL3MNSBIETeizkfNHE7j1LUWaL83wGtzlfn+BxCN\n"\
+                                        "Fqdj\n"\
+                                        "-----END CERTIFICATE-----\n"
 //wallet para
-BCHAR *test_chainmaker_node_url   = "127.0.0.1:12301";
-BCHAR *test_chainmaker_host_name  = "chainmaker.org";
+#define test_chainmaker_node_url    "127.0.0.1:12301"
+#define test_chainmaker_host_name   "chainmaker.org"
 
 //client para
-BCHAR *chain_id              = "chain1";
-BCHAR *org_id                = "wx-org1.chainmaker.org";
+#define chain_id   "chain1"
+#define org_id     "wx-org1.chainmaker.org"
