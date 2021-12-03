ec9d7ce0b53b2117ca9d316a069f345cb3b32a98
Cavalier.liu <1350928576@qq.com>
2021-12-03 11:41:31 +0800

    [chainmaker][#436]add test_01CreateWallet_0005

--- tests/BoAT_chainmaker_linuxDefault/01CreateWallet.c
+++ tests/BoAT_chainmaker_linuxDefault/01CreateWallet.c
@@ -133,6 +133,22 @@ START_TEST(test_01CreateWallet_0004CreateLoadWalletSuccess)
 }
 END_TEST
 
+START_TEST(test_01CreateWallet_0005CreateLoadWalletFailureNoExist) 
+{
+    BSINT32 rtnVal;
+    extern BoatIotSdkContext g_boat_iot_sdk_context;
+
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_CHAINMAKER, "chainmaker_no_exit", NULL, sizeof(BoatHlchainmakerWalletConfig));
+    
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_int_eq(rtnVal, -1);
+
+    /* 2-2. verify the global variables that be affected */
+    ck_assert(g_boat_iot_sdk_context.wallet_list[0].is_used == false);
+}
+END_TEST
 
 Suite *make_wallet_suite(void) 
 {
@@ -149,6 +165,7 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_01CreateWallet_0002CreateOneTimeWalletFailureNullConfig); 
     tcase_add_test(tc_wallet_api, test_01CreateWallet_0003CreatePersistWalletSuccess);
     tcase_add_test(tc_wallet_api, test_01CreateWallet_0004CreateLoadWalletSuccess);
+    tcase_add_test(tc_wallet_api, test_01CreateWallet_0005CreateLoadWalletFailureNoExist);
 
     return s_wallet;
 }
