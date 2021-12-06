5f9cabbbc3da71f911f582f933eba2d209256815
Cavalier.liu <1350928576@qq.com>
2021-12-06 16:30:18 +0800

    [chainmaker][#436]add tests test_03Contract_0002InvokeFailureMethodNull

--- tests/BoAT_chainmaker_linuxDefault/03Contract.c
+++ tests/BoAT_chainmaker_linuxDefault/03Contract.c
@@ -111,13 +111,25 @@ START_TEST(test_03Contract_0001InvokeFailureTxNull)
     BOAT_RESULT        result;
     BoatHlchainmakerTx tx_ptr;
     BoatInvokeReponse  invoke_reponse;
-    result = test_contrct_invoke_prepara(tx_ptr);
-    ck_assert_int_eq(result, BOAT_SUCCESS);
 
-    result = BoatHlchainmakerContractInvoke(NULL, "save","fact", true, &invoke_reponse); ;
+    result = BoatHlchainmakerContractInvoke(NULL, "save", "fact", true, &invoke_reponse); ;
     ck_assert(result == BOAT_ERROR_INVALID_ARGUMENT);
 }
 END_TEST
+
+START_TEST(test_03Contract_0002InvokeFailureMethodNull) 
+{
+    BOAT_RESULT        result;
+    BoatHlchainmakerTx tx_ptr;
+    BoatInvokeReponse  invoke_reponse;
+
+    test_contrct_invoke_prepara(tx_ptr);
+
+    result = BoatHlchainmakerContractInvoke(&tx_ptr, NULL, "fact", true, &invoke_reponse); ;
+    ck_assert(result == BOAT_ERROR_INVALID_ARGUMENT);
+}
+END_TEST
+
 
 Suite *make_contract_suite(void) 
 {
@@ -130,7 +142,8 @@ Suite *make_contract_suite(void)
     /* Add a test case to the Suite */
     suite_add_tcase(s_contract, tc_contract_api);       
     /* Test cases are added to the test set */
-    tcase_add_test(tc_contract_api, test_03Contract_0001InvokeFailureTxNull);  
+    tcase_add_test(tc_contract_api, test_03Contract_0001InvokeFailureTxNull); 
+    tcase_add_test(tc_contract_api, test_03Contract_0002InvokeFailureMethodNull);   
     return s_contract;
 }
 
