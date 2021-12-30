1bc62e9e0692e10ad7db7a9297e65d6950ceb9d2
liuzh <1350928576@qq.com>
2021-12-30 00:36:26 -0800

    [chainmaker][#436]modify py pass parameters

--- tests/BoAT_chainmaker_linuxDefault/01Wallet.c
+++ tests/BoAT_chainmaker_linuxDefault/01Wallet.c
@@ -25,13 +25,13 @@ BOAT_RESULT check_chainmaker_wallet(BoatHlchainmakerWallet *wallet_ptr)
         return result;
     }
 
-    result = strncmp(wallet_ptr->user_cert_info.prikeyCtx.extra_data.value, chainmaker_key_ptr_buf, strlen(chainmaker_key_ptr_buf));
+    result = strncmp(wallet_ptr->user_cert_info.prikeyCtx.extra_data.value, chainmaker_sign_key_buf, strlen(chainmaker_sign_key_buf));
     if (result != 0) 
     {
         return result;
     }
 
-    result = strncmp(wallet_ptr->user_cert_info.cert.field_ptr, chainmaker_cert_ptr_buf, strlen(chainmaker_cert_ptr_buf));
+    result = strncmp(wallet_ptr->user_cert_info.cert.field_ptr, chainmaker_sign_cert_buf, strlen(chainmaker_sign_cert_buf));
     if (result != 0) 
     {
         return result;
@@ -48,13 +48,20 @@ BoatHlchainmakerWalletConfig get_chainmaker_wallet_settings()
     wallet_config.user_prikey_cfg.prikey_type    = BOAT_WALLET_PRIKEY_TYPE_SECP256R1;
     wallet_config.user_prikey_cfg.prikey_format  = BOAT_WALLET_PRIKEY_FORMAT_PKCS;
 
-    wallet_config.user_prikey_cfg.prikey_content.field_ptr = (BUINT8 *)chainmaker_key_ptr_buf;
-    wallet_config.user_prikey_cfg.prikey_content.field_len = strlen(chainmaker_key_ptr_buf) + 1; 
+    wallet_config.user_prikey_cfg.prikey_content.field_ptr = (BUINT8 *)chainmaker_sign_key_buf;
+    wallet_config.user_prikey_cfg.prikey_content.field_len = strlen(chainmaker_sign_key_buf) + 1; 
 
     //set user cert context
-    wallet_config.user_cert_cfg.length = strlen(chainmaker_cert_ptr_buf);
-    memcpy(wallet_config.user_cert_cfg.content, chainmaker_cert_ptr_buf, wallet_config.user_cert_cfg.length);
-    strncpy(wallet_config.node_url_cfg, TEST_CHAINMAKER_NODE_URL, strlen(TEST_CHAINMAKER_NODE_URL));
+    wallet_config.user_cert_cfg.length = strlen(chainmaker_sign_cert_buf);
+    memcpy(wallet_config.user_cert_cfg.content, chainmaker_sign_cert_buf, wallet_config.user_cert_cfg.length);
+    //tls ca cert
+    wallet_config.tls_ca_cert_cfg.length = strlen(chainmaker_ca_cert_buf) + 1;
+    memcpy(wallet_config.tls_ca_cert_cfg.content, chainmaker_ca_cert_buf, wallet_config.tls_ca_cert_cfg.length);
+
+    strncpy(wallet_config.node_url_cfg,  TEST_CHAINMAKER_NODE_URL,  strlen(TEST_CHAINMAKER_NODE_URL));
+    strncpy(wallet_config.host_name_cfg, TEST_CHAINMAKER_HOST_NAME, strlen(TEST_CHAINMAKER_HOST_NAME));
+    strncpy(wallet_config.chain_id_cfg,  TEST_CHAINMAKER_CHAIN_ID,  strlen(TEST_CHAINMAKER_CHAIN_ID));
+    strncpy(wallet_config.org_id_cfg,    TEST_CHAINMAKER_ORG_ID,    strlen(TEST_CHAINMAKER_ORG_ID));
 
     return wallet_config;
 }
