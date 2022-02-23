1c3f093947ac7aaa63c4b50c6c6fbce5c8c65a2b
zhuxun <zhuxun1212@live.com>
2022-02-23 14:23:31 +0800

    test: add ethereum test case
    test_001CreateWallet_0006CreateOneTimeWalletFailureShortSize

--- tests/BoAT_ethereum_linuxDefault/01Wallet.c
+++ tests/BoAT_ethereum_linuxDefault/01Wallet.c
@@ -176,6 +176,25 @@ START_TEST(test_001CreateWallet_0005CreateLoadWalletFailureNoExist)
 }
 END_TEST
 
+START_TEST(test_001CreateWallet_0006CreateOneTimeWalletFailureShortSize) 
+{
+    BSINT32 rtnVal;
+    BoatEthWallet *g_ethereum_wallet_ptr;
+    BoatEthWalletConfig wallet_config = get_ethereum_wallet_settings();
+    extern BoatIotSdkContext g_boat_iot_sdk_context;
+
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_ETHEREUM, NULL, &wallet_config, sizeof(BoatEthWalletConfig) - 10);
+    
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_int_eq(rtnVal, BOAT_ERROR);
+
+    /* 2-2. verify the global variables that be affected */
+    ck_assert(g_boat_iot_sdk_context.wallet_list[0].is_used == false);
+}
+END_TEST
+
 Suite *make_wallet_suite(void) 
 {
     /* Create Suite */
@@ -192,6 +211,7 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0003CreatePersistWalletSuccess);
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0004CreateLoadWalletSuccess);
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0005CreateLoadWalletFailureNoExist);
+    tcase_add_test(tc_wallet_api, test_001CreateWallet_0006CreateOneTimeWalletFailureShortSize);
 
     return s_wallet;
 }
