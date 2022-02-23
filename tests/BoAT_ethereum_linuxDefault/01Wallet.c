695a46fb89119396a21a8eedb63b37027a6bd27f
zhuxun <zhuxun1212@live.com>
2022-02-23 14:14:21 +0800

    test: add ethereum test case
    test_001CreateWallet_0003CreatePersistWalletSuccess

--- tests/BoAT_ethereum_linuxDefault/01Wallet.c
+++ tests/BoAT_ethereum_linuxDefault/01Wallet.c
@@ -102,6 +102,40 @@ START_TEST(test_001CreateWallet_0002CreateOneTimeWalletFailureNullConfig)
 }
 END_TEST
 
+START_TEST(test_001CreateWallet_0003CreatePersistWalletSuccess) 
+{
+    BSINT32 rtnVal;
+    BoatEthWallet *g_ethereum_wallet_ptr;
+
+    BoatIotSdkInit();
+    BoatEthWalletConfig wallet_config = get_ethereum_wallet_settings();
+    extern BoatIotSdkContext g_boat_iot_sdk_context;
+
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_ETHEREUM, "ethereum", &wallet_config, sizeof(BoatEthWalletConfig));
+
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_int_eq(rtnVal, 0);
+    ck_assert_int_eq(access("ethereum", F_OK), 0);
+
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_ETHEREUM, "ethereum", &wallet_config, sizeof(BoatEthWalletConfig));
+    
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_int_eq(rtnVal, 1);
+
+    /* 2-2. verify the global variables that be affected */
+    ck_assert(g_boat_iot_sdk_context.wallet_list[0].is_used == true);
+    ck_assert(g_boat_iot_sdk_context.wallet_list[1].is_used == true);
+
+    g_ethereum_wallet_ptr = BoatGetWalletByIndex(rtnVal);
+    ck_assert(g_ethereum_wallet_ptr != NULL);
+    ck_assert(check_ethereum_wallet(g_ethereum_wallet_ptr) == BOAT_SUCCESS);
+}
+END_TEST
+
 Suite *make_wallet_suite(void) 
 {
     /* Create Suite */
@@ -115,6 +149,7 @@ Suite *make_wallet_suite(void)
     /* Test cases are added to the test set */
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0001CreateOneTimeWalletSuccess);  
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0002CreateOneTimeWalletFailureNullConfig); 
+    tcase_add_test(tc_wallet_api, test_001CreateWallet_0003CreatePersistWalletSuccess);
 
     return s_wallet;
 }
