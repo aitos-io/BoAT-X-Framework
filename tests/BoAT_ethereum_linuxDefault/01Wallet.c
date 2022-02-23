c4dd4f7a1f8da101123757ecb669a376825860d9
zhuxun <zhuxun1212@live.com>
2022-02-23 09:27:13 +0800

    Add ethereum test case test_002InitWallet_0012SetChainIdFailureNullParam

--- tests/BoAT_ethereum_linuxDefault/01Wallet.c
+++ tests/BoAT_ethereum_linuxDefault/01Wallet.c
@@ -285,6 +285,24 @@ START_TEST(test_002InitWallet_0011SetChainIdSuccess)
 }
 END_TEST
 
+START_TEST(test_002InitWallet_0012SetChainIdFailureNullParam)
+{
+    BSINT32 rtnVal;
+    BoatEthWallet *wallet_ptr = BoatMalloc(sizeof(BoatEthWallet));
+    BoatEthWalletConfig wallet_config = get_ethereum_wallet_settings();
+    
+    /* 1. execute unit test */
+    rtnVal = BoatEthWalletSetChainId(NULL, wallet_config.chain_id);
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_int_eq(rtnVal, BOAT_ERROR_COMMON_INVALID_ARGUMENT);
+
+    /* 2-2. verify the global variables that be affected */
+    ck_assert(wallet_ptr->network_info.chain_id == NULL);
+
+}
+END_TEST
+
 Suite *make_wallet_suite(void) 
 {
     /* Create Suite */
@@ -307,6 +325,7 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_002InitWallet_0009SetEIP155CompSuccess);
     tcase_add_test(tc_wallet_api, test_002InitWallet_0010SetEIP155CompFailureNullParam);
     tcase_add_test(tc_wallet_api, test_002InitWallet_0011SetChainIdSuccess);
+    tcase_add_test(tc_wallet_api, test_002InitWallet_0012SetChainIdFailureNullParam);
 
     return s_wallet;
 }
