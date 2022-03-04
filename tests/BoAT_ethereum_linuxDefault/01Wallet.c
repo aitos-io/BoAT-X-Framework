0c996aac8162224703a2276601a3ffa516563fc4
carl-ma <carl.ma@aitos.io>
2022-03-04 16:56:50 +0800

    test: Add ethereum InitEthWalletGenerationKey

--- tests/BoAT_ethereum_linuxDefault/01Wallet.c
+++ tests/BoAT_ethereum_linuxDefault/01Wallet.c
@@ -460,6 +460,26 @@ START_TEST(test_002InitWallet_0012InitEthWalletSuccess)
 }
 END_TEST
 
+START_TEST(test_002InitWallet_0013InitEthWalletGenerationKey)
+{
+    BoatEthWallet *rtnVal;
+    BoatEthWalletConfig walletConfig;
+
+    walletConfig.prikeyCtx_config.prikey_genMode = BOAT_WALLET_PRIKEY_GENMODE_INTERNAL_GENERATION;
+    walletConfig.prikeyCtx_config.prikey_format  = BOAT_WALLET_PRIKEY_FORMAT_NATIVE;
+    walletConfig.prikeyCtx_config.prikey_format = BOAT_WALLET_PRIKEY_TYPE_SECP256K1;
+    walletConfig.eip155_compatibility = BOAT_FALSE;
+
+    /* 1. execute unit test */
+    rtnVal = BoatEthWalletInit(&walletConfig, sizeof(BoatEthWalletConfig));
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_ptr_nonnull(rtnVal);
+
+    /* 2-2. verify the global variables that be affected */ 
+}
+END_TEST
+
 START_TEST(test_002DeleteWallet_0001DeleteWalletFailureNullFleName) 
 {
     BoatWalletDelete(NULL);
@@ -511,6 +531,8 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_002InitWallet_0009InitEthWalletWithNullConfig);
     tcase_add_test(tc_wallet_api, test_002InitWallet_0010InitEthWalletWithSmallerSize);
     tcase_add_test(tc_wallet_api, test_002InitWallet_0011InitEthWalletWithBiggerSize);
+    tcase_add_test(tc_wallet_api, test_002InitWallet_0012InitEthWalletSuccess);
+    tcase_add_test(tc_wallet_api, test_002InitWallet_0013InitEthWalletGenerationKey);
     tcase_add_test(tc_wallet_api, test_003DeleteWallet_0001DeleteWalletFailureNullFleName);
     tcase_add_test(tc_wallet_api, test_003DeleteWallet_0002DeleteWalletFailureNoExistingFile);
     tcase_add_test(tc_wallet_api, test_003DeleteWallet_0003DeleteWalletSucessExistingFile);
