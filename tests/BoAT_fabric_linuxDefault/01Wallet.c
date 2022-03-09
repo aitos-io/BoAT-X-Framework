d94f930a81e0ebc09c9c2e515a1f90488ab98677
zt222 <81662688+zt222@users.noreply.github.com>
2022-03-09 15:17:38 +0800

    feat(fabric tests):
    add the test case of CreateWallet_0004

--- tests/BoAT_fabric_linuxDefault/01Wallet.c
+++ tests/BoAT_fabric_linuxDefault/01Wallet.c
@@ -324,6 +324,23 @@ START_TEST(test_001CreateWallet_0003CreateOneTimeWalletFail_KeyGenmodeErr)
 }
 END_TEST
 
+START_TEST(test_001CreateWallet_0004CreateOneTimeWalletFail_KeyTypeErr) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    BoatHlfabricWalletConfig wallet_config = get_fabric_wallet_settings();
+    extern BoatIotSdkContext g_boat_iot_sdk_context;
+    wallet_config.accountPriKey_config.prikey_type = BOAT_WALLET_PRIKEY_TYPE_UNKNOWN;
+
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_HLFABRIC, NULL, &wallet_config, sizeof(BoatHlfabricWalletConfig));
+
+    
+    ck_assert_int_eq(rtnVal, BOAT_ERROR_WALLET_KEY_TYPE_ERR);
+    BoatIotSdkDeInit();
+}
+END_TEST
+
 
 Suite *make_wallet_suite(void) 
 {
@@ -339,6 +356,7 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0001CreateOneTimeWalletSuccess);
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0002CreateOneTimeWalletSuccess);
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0003CreateOneTimeWalletFail_KeyGenmodeErr);
+    tcase_add_test(tc_wallet_api, test_001CreateWallet_0004CreateOneTimeWalletFail_KeyTypeErr);
 
     return s_wallet;
 }
