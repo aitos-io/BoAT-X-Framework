ba3d63ea52e17594b72cff16f5b8110b3e99884b
xunzhu0507 <zhuxun1212@live.com>
2022-04-11 14:03:19 +0800

    test: add platon tests

--- tests/BoAT_platon_linuxDefault/01Wallet.c
+++ tests/BoAT_platon_linuxDefault/01Wallet.c
@@ -194,6 +194,25 @@ START_TEST(test_001CreateWallet_0005CreateLoadWalletFailureNoExist)
 }
 END_TEST
 
+START_TEST(test_001CreateWallet_0006CreateOneTimeWalletFailureShortSize) 
+{
+    BSINT32 rtnVal;
+    BoatPlatONWalletConfig wallet = get_platon_wallet_settings();
+    extern BoatIotSdkContext g_boat_iot_sdk_context;
+
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_PLATON, NULL, &wallet, sizeof(BoatPlatONWalletConfig) - 10);
+    
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_int_eq(rtnVal, BOAT_ERROR);
+
+    /* 2-2. verify the global variables that be affected */
+    ck_assert(g_boat_iot_sdk_context.wallet_list[0].is_used == false);
+    BoatIotSdkDeInit();
+}
+END_TEST
+
 Suite *make_wallet_suite(void) 
 {
     /* Create Suite */
@@ -210,5 +229,7 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0003CreatePersistWalletSuccess); 
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0004CreateLoadWalletSuccess); 
     tcase_add_test(tc_wallet_api, test_001CreateWallet_0005CreateLoadWalletFailureNoExist); 
+    tcase_add_test(tc_wallet_api, test_001CreateWallet_0006CreateOneTimeWalletFailureShortSize); 
+
     return s_wallet;
 }
