e5d5f0295b17c394c7bdd2209ae72617af924069
xunzhu0507 <zhuxun1212@live.com>
2022-04-21 10:27:55 +0800

    test: add platon test case

--- tests/BoAT_platon_linuxDefault/01Wallet.c
+++ tests/BoAT_platon_linuxDefault/01Wallet.c
@@ -616,6 +616,25 @@ START_TEST(test_002InitWallet_0004SetChainIdFailureNullParam)
 }
 END_TEST
 
+START_TEST(test_002InitWallet_0005SetNodeUrlSuccess)
+{
+    BSINT32 rtnVal;
+    BoatPlatONWallet *wallet_ptr = BoatMalloc(sizeof(BoatPlatONWallet));
+    BoatPlatONWalletConfig wallet = get_platon_wallet_settings();
+    
+    /* 1. execute unit test */
+    wallet_ptr->network_info.node_url_ptr = NULL;
+    rtnVal = BoatEthWalletSetNodeUrl(wallet_ptr, wallet.node_url_str);
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+
+    /* 2-2. verify the global variables that be affected */
+    ck_assert_str_eq(wallet_ptr->network_info.node_url_ptr, wallet.node_url_str);
+    BoatIotSdkDeInit();
+}
+END_TEST
+
 Suite *make_wallet_suite(void) 
 {
     /* Create Suite */
@@ -648,6 +667,7 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_002InitWallet_0002SetEIP155CompFailureNullParam);
     tcase_add_test(tc_wallet_api, test_002InitWallet_0003SetChainIdSuccess);
     tcase_add_test(tc_wallet_api, test_002InitWallet_0004SetChainIdFailureNullParam);
+    tcase_add_test(tc_wallet_api, test_002InitWallet_0005SetNodeUrlSuccess);
 
     return s_wallet;
 }
