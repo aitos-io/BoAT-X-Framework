20b382720ecde2a4ace7777a43a647d192c94578
xunzhu0507 <zhuxun1212@live.com>
2022-04-22 10:55:02 +0800

    test: fix platon test case name

--- tests/BoAT_platon_linuxDefault/01Wallet.c
+++ tests/BoAT_platon_linuxDefault/01Wallet.c
@@ -747,6 +747,21 @@ START_TEST(test_002InitWallet_0010InitPlatONWalletWithSmallerSize)
 }
 END_TEST
 
+START_TEST(test_002InitWallet_0011InitPlatONWalletWithBiggerSize)
+{
+    BoatPlatONWallet *rtnVal;
+    BoatPlatONWalletConfig walletConfig = {0};
+
+    /* 1. execute unit test */
+    rtnVal = BoatEthWalletInit(&walletConfig, sizeof(BoatPlatONWalletConfig) + 1);
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_ptr_eq(rtnVal,NULL);
+
+    /* 2-2. verify the global variables that be affected */ 
+}
+END_TEST
+
 Suite *make_wallet_suite(void) 
 {
     /* Create Suite */
@@ -785,6 +800,7 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_002InitWallet_0008SetNodeUrlFailureNodeUrlOutOfLimit);
     tcase_add_test(tc_wallet_api, test_002InitWallet_0009InitPlatONWalletWithNullConfig);
     tcase_add_test(tc_wallet_api, test_002InitWallet_0010InitEthWalletWithSmallerSize);
+    tcase_add_test(tc_wallet_api, test_002InitWallet_0011InitPlatONWalletWithBiggerSize);
   
     return s_wallet;
 }
