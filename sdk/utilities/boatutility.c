42431d2c8b8e4cd9831aa60756f216c208bfd3f5
zt222 <81662688+zt222@users.noreply.github.com>
2022-04-24 09:36:36 +0800

    fix:
    this method is only used by hw chain, move it to api_hw_bcs.c #558

--- sdk/utilities/boatutility.c
+++ sdk/utilities/boatutility.c
@@ -679,59 +679,6 @@ BOAT_RESULT BoatFieldVariable_malloc_size_expand(BoatFieldVariable *mem, BUINT32
 	return BOAT_SUCCESS;
 }
 
-#if (BOAT_HWBCS_TLS_SUPPORT == 1)
-#include "mbedtls/x509_crt.h"
-#include "mbedtls/oid.h"
-size_t Utility_find_oid_value_in_name(const mbedtls_x509_name *name, const char* target_short_name, char *value, size_t value_length)
-{
-    const char *short_name = NULL;
-    bool found = false;
-    size_t retval = 0;
-
-    while((name != NULL) && !found)
-    {
-        // if there is no data for this name go to the next one
-        if(!name->oid.p)
-        {
-            name = name->next;
-            continue;
-        }
-
-        int ret = mbedtls_oid_get_attr_short_name(&name->oid, &short_name);
-        if((ret == 0) && (strcmp(short_name, target_short_name) == 0))
-        {
-            found = true;
-        }
-
-        if(found)
-        {
-            size_t bytes_to_write = (name->val.len >= value_length) ? value_length - 1 : name->val.len;
-
-            for(size_t i = 0; i < bytes_to_write; i++)
-            {
-                char c = name->val.p[i];
-                if (c < 32 || c == 127 || (c > 128 && c < 160))
-                {
-                    value[i] = '?';
-                } else
-                {
-                    value[i] = c;
-                }
-            }
-
-            // null terminate
-            value[bytes_to_write] = 0;
-
-            retval = name->val.len;
-        }
-
-        name = name->next;
-    }
-
-    return retval;
-}
-#endif
-
 
 
 char *Utility_itoa(int num, char *str, int radix)
