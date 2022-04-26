a561cd845f90051576c34f7a985670417159d9e1
carl-ma <carl.ma@aitos.io>
2022-04-26 21:18:16 +0800

    test: Add test_008Contract_0007SetAndGetIntArrayContractFuction

--- tests/BoAT_ethereum_linuxDefault/04General.c
+++ tests/BoAT_ethereum_linuxDefault/04General.c
@@ -401,6 +401,43 @@ START_TEST(test_008Contract_0006SetAndGetAddressContractFuction)
 }
 END_TEST
 
+START_TEST(test_008Contract_0007SetAndGetIntArrayContractFuction)
+{
+    BoatEthTx tx_ctx;
+    BoatEthWallet *rtnVal;
+    BCHAR *result_str;
+    BOAT_RESULT result;
+    BSINT32 bs[10];
+    BUINT32 i;
+    BoatFieldVariable parse_result = {NULL, 0};
+    BoatEthWalletConfig walletConfig = get_ethereum_wallet_settings();
+
+    /* 1. execute unit test */
+    rtnVal = BoatEthWalletInit(&walletConfig, sizeof(BoatEthWalletConfig));
+    ck_assert_ptr_ne(rtnVal, NULL);
+
+    result = BoatEthTxInit(rtnVal, &tx_ctx, BOAT_TRUE, NULL,
+						   "0x333333",
+						   (BCHAR *)TEST_CONTRACT_ADDRESS);
+    ck_assert_int_eq(result, BOAT_SUCCESS);   
+
+    for (i = 0; i < 10; i++)  
+    {
+        ba[i] = 0 - i;
+    }      
+
+    result_str = TestABIContract_setIntArray(&tx_ctx, bs, 10);
+    ck_assert_ptr_ne(rtnVal, NULL);
+
+    result_str = TestABIContract_getIntArray(&tx_ctx);
+    ck_assert_ptr_ne(rtnVal, NULL);
+    result = BoatEthParseRpcResponseStringResult(result_str, &parse_result);
+    ck_assert_int_eq(result, BOAT_SUCCESS);   
+
+    BoatLog(BOAT_LOG_NORMAL, "readListByIndex returns: %s", parse_result.field_ptr);
+}
+END_TEST
+
 Suite *make_general_suite(void)
 {
     /* Create Suite */
@@ -430,6 +467,8 @@ Suite *make_general_suite(void)
     tcase_add_test(tc_general_api, test_008Contract_0004SetAndGetStringContractFuction); 
     tcase_add_test(tc_general_api, test_008Contract_0005SetAndGetBytesContractFuction);
     tcase_add_test(tc_general_api, test_008Contract_0006SetAndGetAddressContractFuction);
+    tcase_add_test(tc_general_api, test_008Contract_0007SetAndGetIntArrayContractFuction);
+    
     
     return s_general;
 }
