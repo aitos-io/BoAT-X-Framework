eb067a7a90418c21eeb3ff6966f9429629165ad2
zt222 <81662688+zt222@users.noreply.github.com>
2022-05-05 11:27:19 +0800

    feat(fabric test):
    add fabric test case "TxSetNetworkInfo_NodesCfg_layoutNumIs0" #715

--- tests/BoAT_fabric_linuxDefault/02Transaction.c
+++ tests/BoAT_fabric_linuxDefault/02Transaction.c
@@ -148,6 +148,7 @@ START_TEST(test_002Transaction_0007SetNetworkInfoSuccess)
 }
 END_TEST
 
+
 START_TEST(test_002Transaction_0008TxSetNetworkInfo_Wallet_NULL) 
 {
     BSINT32 rtnVal;
@@ -162,6 +163,27 @@ START_TEST(test_002Transaction_0008TxSetNetworkInfo_Wallet_NULL)
     rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", NULL);
     ck_assert_int_eq(rtnVal, BOAT_ERROR_COMMON_INVALID_ARGUMENT);
     rtnVal = BoatHlfabricWalletSetNetworkInfo(NULL, wallet_config.nodesCfg);
+	ck_assert_int_eq(rtnVal, BOAT_ERROR_COMMON_INVALID_ARGUMENT);
+    BoatIotSdkDeInit();
+    fabricWalletConfigFree(wallet_config);
+}
+END_TEST
+
+START_TEST(test_002Transaction_0009TxSetNetworkInfo_NodesCfg_layoutNumIs0) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricTx tx_ptr;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    BoatHlfabricWalletConfig wallet_config = get_fabric_wallet_settings();
+    BoatIotSdkInit();
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_HLFABRIC, NULL, &wallet_config, sizeof(BoatHlfabricWalletConfig));
+    g_fabric_wallet_ptr = BoatGetWalletByIndex(rtnVal);
+
+    rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    wallet_config.nodesCfg.endorserLayoutNum = 0;
+    rtnVal = BoatHlfabricWalletSetNetworkInfo(tx_ptr.wallet_ptr, wallet_config.nodesCfg);
 	ck_assert_int_eq(rtnVal, BOAT_ERROR_COMMON_INVALID_ARGUMENT);
     BoatIotSdkDeInit();
     fabricWalletConfigFree(wallet_config);
@@ -187,6 +209,7 @@ Suite *make_transaction_suite(void)
     tcase_add_test(tc_transaction_api, test_002Transaction_0006TxInit_orgName_NULL);
     tcase_add_test(tc_transaction_api, test_002Transaction_0007SetNetworkInfoSuccess);
     tcase_add_test(tc_transaction_api, test_002Transaction_0008TxSetNetworkInfo_Wallet_NULL);
+    tcase_add_test(tc_transaction_api, test_002Transaction_0009TxSetNetworkInfo_NodesCfg_layoutNumIs0);
     
 
     return s_transaction;
