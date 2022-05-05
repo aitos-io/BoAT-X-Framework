926b58995708cabfbb30b2c4f152e7f81ff27482
zt222 <81662688+zt222@users.noreply.github.com>
2022-05-05 11:56:45 +0800

    feat(fabric test):
    add fabric test case "TxSetArgs_argsMoreThanLimit" #721

--- tests/BoAT_fabric_linuxDefault/02Transaction.c
+++ tests/BoAT_fabric_linuxDefault/02Transaction.c
@@ -311,6 +311,32 @@ START_TEST(test_002Transaction_0014TxSetArgs_Txptr_NULL)
 }
 END_TEST
 
+START_TEST(test_002Transaction_0015TxSetArgs_argsMoreThanLimit) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricTx tx_ptr;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    BoatHlfabricWalletConfig wallet_config = get_fabric_wallet_settings();
+    BoatIotSdkInit();
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_HLFABRIC, NULL, &wallet_config, sizeof(BoatHlfabricWalletConfig));
+    g_fabric_wallet_ptr = BoatGetWalletByIndex(rtnVal);
+
+    rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricWalletSetNetworkInfo(tx_ptr.wallet_ptr, wallet_config.nodesCfg);
+	ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    long int timesec = 0;
+	time(&timesec);
+	rtnVal = BoatHlfabricTxSetTimestamp(&tx_ptr, timesec, 0);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricTxSetArgs(&tx_ptr, "invoke", "a", "b", "10", "c","d","e","f","c","d","e","f");
+    ck_assert_int_eq(rtnVal, BOAT_ERROR_COMMON_OUT_OF_MEMORY);
+    BoatIotSdkDeInit();
+    fabricWalletConfigFree(wallet_config);
+}
+END_TEST
+
 Suite *make_transaction_suite(void) 
 {
     /* Create Suite */
@@ -336,6 +362,7 @@ Suite *make_transaction_suite(void)
     tcase_add_test(tc_transaction_api, test_002Transaction_0012TxSetTimestamp_TxPtr_NULL);
     tcase_add_test(tc_transaction_api, test_002Transaction_0013TxSetArgs_Success);
     tcase_add_test(tc_transaction_api, test_002Transaction_0014TxSetArgs_Txptr_NULL);
+    tcase_add_test(tc_transaction_api, test_002Transaction_0015TxSetArgs_argsMoreThanLimit);
 
     return s_transaction;
 }
