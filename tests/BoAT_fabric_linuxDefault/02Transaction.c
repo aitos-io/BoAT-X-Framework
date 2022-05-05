07230b80dd3d679a6d526575831e2c1c10d061e6
zt222 <81662688+zt222@users.noreply.github.com>
2022-05-05 11:31:40 +0800

    feat(fabric test):
    add fabric test case "TxSetNetworkInfo_NodesCfg_orderCfg_NumIs0" #716

--- tests/BoAT_fabric_linuxDefault/02Transaction.c
+++ tests/BoAT_fabric_linuxDefault/02Transaction.c
@@ -190,6 +190,27 @@ START_TEST(test_002Transaction_0009TxSetNetworkInfo_NodesCfg_layoutNumIs0)
 }
 END_TEST
 
+START_TEST(test_002Transaction_0010TxSetNetworkInfo_NodesCfg_orderCfg_NumIs0) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricTx tx_ptr;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    BoatHlfabricWalletConfig wallet_config = get_fabric_wallet_settings();
+    BoatIotSdkInit();
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_HLFABRIC, NULL, &wallet_config, sizeof(BoatHlfabricWalletConfig));
+    g_fabric_wallet_ptr = BoatGetWalletByIndex(rtnVal);
+
+    rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    wallet_config.nodesCfg.orderCfg.endorserNumber = 0;
+    rtnVal = BoatHlfabricWalletSetNetworkInfo(tx_ptr.wallet_ptr, wallet_config.nodesCfg);
+	ck_assert_int_eq(rtnVal, BOAT_ERROR_COMMON_INVALID_ARGUMENT);
+    BoatIotSdkDeInit();
+    fabricWalletConfigFree(wallet_config);
+}
+END_TEST
+
 Suite *make_transaction_suite(void) 
 {
     /* Create Suite */
@@ -210,6 +231,7 @@ Suite *make_transaction_suite(void)
     tcase_add_test(tc_transaction_api, test_002Transaction_0007SetNetworkInfoSuccess);
     tcase_add_test(tc_transaction_api, test_002Transaction_0008TxSetNetworkInfo_Wallet_NULL);
     tcase_add_test(tc_transaction_api, test_002Transaction_0009TxSetNetworkInfo_NodesCfg_layoutNumIs0);
+    tcase_add_test(tc_transaction_api, test_002Transaction_0010TxSetNetworkInfo_NodesCfg_orderCfg_NumIs0);
     
 
     return s_transaction;
