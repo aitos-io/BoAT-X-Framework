5e2662f55e029d75ce3dc964764b1ffad127af62
zt222 <81662688+zt222@users.noreply.github.com>
2022-05-11 11:56:04 +0800

    fix(fabric test):
    add fabric test case "TxInvokeFail_Args_Lack1" #755

--- tests/BoAT_fabric_linuxDefault/02Transaction.c
+++ tests/BoAT_fabric_linuxDefault/02Transaction.c
@@ -421,6 +421,34 @@ START_TEST(test_002Transaction_0018TxInvokeFail_Args_NULL)
 }
 END_TEST
 
+START_TEST(test_002Transaction_0019TxInvokeFail_Args_Lack1) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricTx tx_ptr;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    BoatHlfabricWalletConfig wallet_config = get_fabric_wallet_settings();
+    BoatIotSdkInit();
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_HLFABRIC, NULL, &wallet_config, sizeof(BoatHlfabricWalletConfig));
+    g_fabric_wallet_ptr = BoatGetWalletByIndex(rtnVal);
+
+    rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricWalletSetNetworkInfo(tx_ptr.wallet_ptr, wallet_config.nodesCfg);
+	ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    long int timesec = 0;
+	time(&timesec);
+	rtnVal = BoatHlfabricTxSetTimestamp(&tx_ptr, timesec, 0);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricTxSetArgs(&tx_ptr, "invoke", "a", "b", NULL);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricTxSubmit(&tx_ptr); 
+    ck_assert_int_eq(rtnVal, BOAT_ERROR);
+    BoatIotSdkDeInit();
+    fabricWalletConfigFree(wallet_config);
+}
+END_TEST
+
 Suite *make_transaction_suite(void) 
 {
     /* Create Suite */
@@ -450,6 +478,7 @@ Suite *make_transaction_suite(void)
     tcase_add_test(tc_transaction_api, test_002Transaction_0016TxInvoke_Success);
     tcase_add_test(tc_transaction_api, test_002Transaction_0017TxInvokeFail_Txptr_NULL);
     tcase_add_test(tc_transaction_api, test_002Transaction_0018TxInvokeFail_Args_NULL);
+    tcase_add_test(tc_transaction_api, test_002Transaction_0019TxInvokeFail_Args_Lack1);
 
     return s_transaction;
 }
