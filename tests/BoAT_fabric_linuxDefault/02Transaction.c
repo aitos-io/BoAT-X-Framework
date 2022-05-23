9c2f65d9062427b0d1532df0d2e5b74c0c17c524
zt222 <81662688+zt222@users.noreply.github.com>
2022-05-23 16:03:58 +0800

    feat(fabric test):
    add fabric test case "TxQueryFail_arg2_NULL" #793

--- tests/BoAT_fabric_linuxDefault/02Transaction.c
+++ tests/BoAT_fabric_linuxDefault/02Transaction.c
@@ -698,6 +698,35 @@ START_TEST(test_002Transaction_0027TxQueryFail_arg2_ERR)
 }
 END_TEST
 
+START_TEST(test_002Transaction_0028TxQueryFail_arg2_NULL) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricTx tx_ptr;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    BoatHlfabricWalletConfig wallet_config = get_fabric_wallet_settings();
+    BoatIotSdkInit();
+    /* 1. execute unit test */
+    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_HLFABRIC, NULL, &wallet_config, sizeof(BoatHlfabricWalletConfig));
+    g_fabric_wallet_ptr = BoatGetWalletByIndex(rtnVal);
+
+    rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricWalletSetNetworkInfo(tx_ptr.wallet_ptr, wallet_config.nodesCfg);
+	ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    long int timesec = 0;
+	time(&timesec);
+	rtnVal = BoatHlfabricTxSetTimestamp(&tx_ptr, timesec, 0);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricTxSetArgs(&tx_ptr, "query", NULL, NULL);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricTxSubmit(&tx_ptr); 
+    ck_assert_int_eq(rtnVal, BOAT_ERROR);
+    BoatHlfabricTxDeInit(&tx_ptr);
+    BoatIotSdkDeInit();
+    fabricWalletConfigFree(wallet_config);
+}
+END_TEST
+
 
 Suite *make_transaction_suite(void) 
 {
@@ -737,6 +766,7 @@ Suite *make_transaction_suite(void)
     tcase_add_test(tc_transaction_api, test_002Transaction_0025TxQueryFail_Args_NULL);
     tcase_add_test(tc_transaction_api, test_002Transaction_0026TxQueryFail_Method_ERR);
     tcase_add_test(tc_transaction_api, test_002Transaction_0027TxQueryFail_arg2_ERR);
+    tcase_add_test(tc_transaction_api, test_002Transaction_0028TxQueryFail_arg2_NULL);
 
     return s_transaction;
 }
