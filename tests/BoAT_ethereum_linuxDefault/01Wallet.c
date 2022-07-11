f24d97dcd5c2caff9eb847620c3e58132ba82b57
xunzhu0507 <zhuxun1212@live.com>
2022-07-11 19:20:52 +0800

    fix:fix ethereum test case(test_002InitWallet_0003SetChainIdSuccess)

--- tests/BoAT_ethereum_linuxDefault/01Wallet.c
+++ tests/BoAT_ethereum_linuxDefault/01Wallet.c
@@ -635,7 +635,7 @@ START_TEST(test_002InitWallet_0003SetChainIdSuccess)
 
     /* 2-2. verify the global variables that be affected */
     ck_assert(wallet_ptr->network_info.chain_id == wallet.chain_id);
-    BoatIotSdkDeInit();
+    BoatFree(wallet_ptr); 
 }
 END_TEST
 
