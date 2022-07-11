0202a9ca9921a31efbca83fe74b56d2e07f8946b
xunzhu0507 <zhuxun1212@live.com>
2022-07-11 19:20:10 +0800

    fix:fix ethereum test case(test_002InitWallet_0002SetEIP155CompFailureNullParam)

--- tests/BoAT_ethereum_linuxDefault/01Wallet.c
+++ tests/BoAT_ethereum_linuxDefault/01Wallet.c
@@ -609,7 +609,6 @@ END_TEST
 START_TEST(test_002InitWallet_0002SetEIP155CompFailureNullParam)
 {
     BSINT32 rtnVal;
-    BoatEthWallet *wallet_ptr = BoatMalloc(sizeof(BoatEthWallet));
     BoatEthWalletConfig wallet = get_ethereum_wallet_settings();
     
     /* 1. execute unit test */
@@ -619,8 +618,6 @@ START_TEST(test_002InitWallet_0002SetEIP155CompFailureNullParam)
     ck_assert_int_eq(rtnVal, BOAT_ERROR_COMMON_INVALID_ARGUMENT);
 
     /* 2-2. verify the global variables that be affected */
-    ck_assert(wallet_ptr->network_info.eip155_compatibility == BOAT_FALSE);
-    BoatIotSdkDeInit();
 }
 END_TEST
 
