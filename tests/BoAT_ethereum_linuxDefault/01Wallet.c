81c61b554dd71d65a60ea21fde3ecd5ed21af895
xunzhu0507 <zhuxun1212@live.com>
2022-07-11 15:59:44 +0800

    fix:fix ethereum test case(test_001CreateWallet_0014UnloadInexistentWallet)

--- tests/BoAT_ethereum_linuxDefault/01Wallet.c
+++ tests/BoAT_ethereum_linuxDefault/01Wallet.c
@@ -388,6 +388,7 @@ END_TEST
 START_TEST(test_001CreateWallet_0014UnloadInexistentWallet)
 {
     BSINT32 rtnVal;
+    BoatIotSdkInit();
     BoatEthWalletConfig wallet = get_ethereum_wallet_settings();
     extern BoatIotSdkContext g_boat_iot_sdk_context;
     wallet.prikeyCtx_config.prikey_genMode = BOAT_WALLET_PRIKEY_GENMODE_INTERNAL_GENERATION;
