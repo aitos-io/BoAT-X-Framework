fa711b4e69b22975f30e4d9f9d412d32af5da3e6
xunzhu0507 <zhuxun1212@live.com>
2022-07-20 11:00:55 +0800

    fix: add ck_assert in 'test_006GetBalance_0002GetWalletDefaultAddressSuccess'

--- tests/BoAT_ethereum_linuxDefault/03Transaction.c
+++ tests/BoAT_ethereum_linuxDefault/03Transaction.c
@@ -64,7 +64,8 @@ START_TEST(test_006GetBalance_0002GetWalletDefaultAddressSuccess)
 
     BoatIotSdkInit();
 
-    ethereumWalletPrepare();
+    result = ethereumWalletPrepare();
+    ck_assert(rtnVal == BOAT_SUCCESS);
 
     result = BoatEthTxInit(g_ethereum_wallet_ptr, &tx_ctx, BOAT_TRUE, NULL,
                            "0x333333",
@@ -75,6 +76,7 @@ START_TEST(test_006GetBalance_0002GetWalletDefaultAddressSuccess)
     cur_balance_wei = BoatEthWalletGetBalance(g_ethereum_wallet_ptr, NULL);
 	result          = BoatEthParseRpcResponseStringResult(cur_balance_wei, &parse_result);
 
+    ck_assert_ptr_ne(parse_result.field_ptr, NULL);
     ck_assert_int_eq(result, BOAT_SUCCESS);
 
     BoatFree(parse_result.field_ptr);
