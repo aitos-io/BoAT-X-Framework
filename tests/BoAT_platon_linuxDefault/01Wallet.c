c32f9f04ea9a027c743de6d6e0b17b7939a77c8e
xunzhu0507 <zhuxun1212@live.com>
2022-08-03 10:13:42 +0800

    fix: delete BoatIotSdkDeInit(); in test_001CreateWallet_0002CreateOneTimeWalletFailureNullConfig

--- tests/BoAT_platon_linuxDefault/01Wallet.c
+++ tests/BoAT_platon_linuxDefault/01Wallet.c
@@ -115,7 +115,6 @@ START_TEST(test_001CreateWallet_0002CreateOneTimeWalletFailureNullConfig)
 
     /* 3-2. verify the global variables that be affected */
     ck_assert(g_boat_iot_sdk_context.wallet_list[0].is_used == false);
-    BoatIotSdkDeInit();
 }
 END_TEST
 
