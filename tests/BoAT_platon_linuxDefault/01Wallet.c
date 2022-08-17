5dd0e123dedfc1f4f0ef7c04a6341fb61082fb35
xunzhu0507 <zhuxun1212@live.com>
2022-08-17 13:54:24 +0800

    fix: add BoatIotSdkInit(); in test_001CreateWallet_0009CreateWalletWithInternalGeneration

--- tests/BoAT_platon_linuxDefault/01Wallet.c
+++ tests/BoAT_platon_linuxDefault/01Wallet.c
@@ -250,6 +250,7 @@ END_TEST
 START_TEST(test_001CreateWallet_0009CreateWalletWithInternalGeneration)
 {
     BSINT32 rtnVal;
+    BoatIotSdkInit();
     BoatPlatONWalletConfig wallet = get_platon_wallet_settings();
     extern BoatIotSdkContext g_boat_iot_sdk_context;
     wallet.prikeyCtx_config.prikey_genMode = BOAT_WALLET_PRIKEY_GENMODE_INTERNAL_GENERATION;
