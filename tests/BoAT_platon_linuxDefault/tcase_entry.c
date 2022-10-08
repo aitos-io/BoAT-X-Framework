f7e2ab265a0c208920ba857ec077221db95e3377
xunzhu0507 <zhuxun1212@live.com>
2022-10-08 10:11:34 +0800

    fix: platon test don't add transactions_suite in tcase_entry.c fix the issue #1175 teambition task id: BoATE-738

--- tests/BoAT_platon_linuxDefault/tcase_entry.c
+++ tests/BoAT_platon_linuxDefault/tcase_entry.c
@@ -22,9 +22,8 @@
 
 /* extern suite declaration */
 extern Suite *make_wallet_suite(void);
-// extern Suite *make_parameters_suite(void);
-// extern Suite *make_transactions_suite(void);
-// extern Suite *make_contract_suite(void);
+extern Suite *make_parameters_suite(void);
+extern Suite *make_transactions_suite(void);
 
 char platon_private_key_buf[1024];
 
@@ -71,8 +70,8 @@ int main(int argc, char *argv[])
     /* new adding test suite should create in here */
     Suite *suite_wallet    = make_wallet_suite();
     Suite *suite_paramters = make_parameters_suite();
-    // Suite *suite_transaction = make_transactions_suite();
-    // Suite *suite_contract  = make_contract_suite();
+    Suite *suite_transaction = make_transactions_suite();
+
     read_key_content(platon_private_key_buf);
 
     /* create srunner and add first suite to it.
@@ -84,8 +83,7 @@ int main(int argc, char *argv[])
     /* add other suite to srunner, more test suite should be add in here */
     srunner_add_suite(sr, suite_paramters);
 
-    // srunner_add_suite(sr, suite_transaction);
-//   srunner_add_suite(sr, suite_contract);
+    srunner_add_suite(sr, suite_transaction);
 
     /* start to run all test case */
     srunner_run_all(sr, CK_NORMAL);
