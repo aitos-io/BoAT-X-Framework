b0ea91bb4b19998b7c1cbea2d2ad3700ac0010ad
xunzhu0507 <zhuxun1212@live.com>
2022-10-09 15:01:39 +0800

    test: add platone test case test_002InitWallet_0009InitPlatoneWalletWithNullConfig fix the issue aitos-io#1176 teambition task id: BoATE-744

--- tests/BoAT_platone_linuxDefault/01Wallet.c
+++ tests/BoAT_platone_linuxDefault/01Wallet.c
@@ -759,6 +759,20 @@ START_TEST(test_002InitWallet_0008SetNodeUrlFailureNodeUrlOutOfLimit)
 }
 END_TEST
 
+START_TEST(test_002InitWallet_0009InitPlatoneWalletWithNullConfig)
+{
+    BoatPlatoneWallet *rtnVal;
+
+    /* 1. execute unit test */
+    rtnVal = BoatPlatoneWalletInit(NULL, sizeof(BoatPlatoneWalletConfig));
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_ptr_eq(rtnVal,NULL);
+
+    /* 2-2. verify the global variables that be affected */ 
+}
+END_TEST
+
 Suite *make_wallet_suite(void) 
 {
     /* Create Suite */
@@ -797,6 +811,7 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_002InitWallet_0006SetNodeUrlFailureNullParam);  
     tcase_add_test(tc_wallet_api, test_002InitWallet_0007SetNodeUrlFailureErrorNodeUrlFormat);  
     tcase_add_test(tc_wallet_api, test_002InitWallet_0008SetNodeUrlFailureNodeUrlOutOfLimit);  
+    tcase_add_test(tc_wallet_api, test_002InitWallet_0009InitPlatoneWalletWithNullConfig);  
 
     return s_wallet;
 }
