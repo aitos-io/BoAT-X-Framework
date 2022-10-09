e395b24e0344d08fb074771bf6e05420b432d8d5
xunzhu0507 <zhuxun1212@live.com>
2022-10-09 13:46:56 +0800

    test: add platone test case test_002InitWallet_0005SetNodeUrlSuccess fix the issue aitos-io#1176 teambition task id: BoATE-744

--- tests/BoAT_platone_linuxDefault/01Wallet.c
+++ tests/BoAT_platone_linuxDefault/01Wallet.c
@@ -657,6 +657,25 @@ START_TEST(test_002InitWallet_0004SetChainIdFailureNullParam)
 }
 END_TEST
 
+START_TEST(test_002InitWallet_0005SetNodeUrlSuccess)
+{
+    BSINT32 rtnVal;
+    BoatPlatoneWallet *wallet_ptr = BoatMalloc(sizeof(BoatPlatoneWallet));
+    BoatPlatoneWalletConfig wallet = get_platone_wallet_settings();
+    
+    /* 1. execute unit test */
+    wallet_ptr->network_info.node_url_ptr = NULL;
+    rtnVal = BoatPlatoneWalletSetNodeUrl(wallet_ptr, wallet.node_url_str);
+    /* 2. verify test result */
+    /* 2-1. verify the return value */
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+
+    /* 2-2. verify the global variables that be affected */
+    ck_assert_str_eq(wallet_ptr->network_info.node_url_ptr, wallet.node_url_str);
+    BoatPlatoneWalletDeInit(wallet_ptr);
+}
+END_TEST
+
 Suite *make_wallet_suite(void) 
 {
     /* Create Suite */
@@ -691,6 +710,7 @@ Suite *make_wallet_suite(void)
     tcase_add_test(tc_wallet_api, test_002InitWallet_0002SetEIP155CompFailureNullParam);  
     tcase_add_test(tc_wallet_api, test_002InitWallet_0003SetChainIdSuccess);  
     tcase_add_test(tc_wallet_api, test_002InitWallet_0004SetChainIdFailureNullParam);  
+    tcase_add_test(tc_wallet_api, test_002InitWallet_0005SetNodeUrlSuccess);  
 
     return s_wallet;
 }
