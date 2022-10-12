12883affb3f8351d53cad3a764a97a6c3160865f
xunzhu0507 <zhuxun1212@live.com>
2022-10-12 10:54:36 +0800

    test: add platon test case test_007Transfer_0003~0004 test_008ParseJson_0001~0002 fix the issue aitos-io#1141 teambition task id BoATE-706

--- tests/BoAT_platon_linuxDefault/03Transaction.c
+++ tests/BoAT_platon_linuxDefault/03Transaction.c
@@ -98,7 +98,7 @@ START_TEST(test_007Transfer_0001TransferSuccess)
     ck_assert(result == BOAT_SUCCESS);
 
     result = BoatPlatONTxInit(g_platon_wallet_ptr, &tx_ctx, TEST_IS_SYNC_TX, NULL,
-                           "0x333333",
+                           TEST_GAS_LIMIT,
                            (BCHAR *)TEST_RECIPIENT_ADDRESS, hrp);
     ck_assert_int_eq(result, BOAT_SUCCESS);
 
@@ -120,7 +120,7 @@ START_TEST(test_007Transfer_0002TransferFailureNullParam)
     ck_assert(result == BOAT_SUCCESS);
 
     result = BoatPlatONTxInit(g_platon_wallet_ptr, &tx_ctx, TEST_IS_SYNC_TX, NULL,
-                           "0x333333",
+                           TEST_GAS_LIMIT,
                            (BCHAR *)TEST_RECIPIENT_ADDRESS, hrp);
     ck_assert_int_eq(result, BOAT_SUCCESS);
 
@@ -131,26 +131,26 @@ START_TEST(test_007Transfer_0002TransferFailureNullParam)
 }
 END_TEST
 
-START_TEST(test_007Transfer_0003TransferWithSpecifyChainIDSuccess) 
+START_TEST(test_007Transfer_0003BoatPlatonParseRpcResponseResultFail)
 {
-    BSINT32 rtnVal;
+    BOAT_RESULT result;
+    result = BoatPlatONParseRpcResponseResult(NULL,NULL,NULL);
+    ck_assert(result = BOAT_ERROR_COMMON_INVALID_ARGUMENT);
+}
+END_TEST
+
+START_TEST(test_007Transfer_0004TransferSuccessWithoutSync) 
+{
     BOAT_RESULT result;
     BoatPlatONTx tx_ctx;
 
-    BoatIotSdkInit();
+    BoatIotSdkInit();    
 
-    BoatPlatONWalletConfig wallet = get_platon_wallet_settings();
-    
-    wallet.eip155_compatibility = BOAT_TRUE;
-    wallet.chain_id = TEST_PLATON_CHAIN_ID;
-    /* 1. execute unit test */
-    rtnVal = BoatWalletCreate(BOAT_PROTOCOL_PLATON, NULL, &wallet, sizeof(BoatPlatONWalletConfig));
-    ck_assert_int_eq(rtnVal, 0);
+    result = platonWalletPrepare();
+    ck_assert(result == BOAT_SUCCESS);
 
-    g_platon_wallet_ptr = BoatGetWalletByIndex(rtnVal);
-
-    result = BoatPlatONTxInit(g_platon_wallet_ptr, &tx_ctx, TEST_IS_SYNC_TX, NULL,
-                           "0x333333",
+    result = BoatPlatONTxInit(g_platon_wallet_ptr, &tx_ctx, BOAT_FALSE, NULL,
+                           TEST_GAS_LIMIT,
                            (BCHAR *)TEST_RECIPIENT_ADDRESS, hrp);
     ck_assert_int_eq(result, BOAT_SUCCESS);
 
@@ -158,6 +158,37 @@ START_TEST(test_007Transfer_0003TransferWithSpecifyChainIDSuccess)
     ck_assert_int_eq(result, BOAT_SUCCESS);
 
     BoatIotSdkDeInit();
+}
+END_TEST
+
+START_TEST(test_008ParseJson_0001ParseJsonFailureNullParam)
+{
+    BOAT_RESULT result;
+    BCHAR *json_string= "111";
+    BCHAR *child_name = "";
+    BoatFieldVariable *result_out = {NULL, 0};
+
+    result = platon_parse_json_result(NULL, child_name, result_out);
+    ck_assert(result = BOAT_ERROR_COMMON_INVALID_ARGUMENT);
+
+    result = platon_parse_json_result(json_string, NULL, result_out);
+    ck_assert(result = BOAT_ERROR_COMMON_INVALID_ARGUMENT);
+
+    result = platon_parse_json_result(json_string, child_name, NULL);
+    ck_assert(result = BOAT_ERROR_COMMON_INVALID_ARGUMENT);        
+}
+END_TEST
+
+START_TEST(test_008ParseJson_0002ObtainResultObjectFailure)
+{
+    BOAT_RESULT result;
+    BCHAR *json_string= "111";
+    BCHAR *child_name = "";
+    BoatFieldVariable *result_out = {"1", 1};
+
+    result = platon_parse_json_result(json_string, child_name, result_out);
+    ck_assert(result = BOAT_ERROR_COMMON_INVALID_ARGUMENT);
+   
 }
 END_TEST
 
@@ -182,7 +213,11 @@ Suite *make_transactions_suite(void)
 
     tcase_add_test(tc_transaction_api, test_007Transfer_0001TransferSuccess); 
     tcase_add_test(tc_transaction_api, test_007Transfer_0002TransferFailureNullParam); 
-    tcase_add_test(tc_transaction_api, test_007Transfer_0003TransferWithSpecifyChainIDSuccess); 
-    
+    tcase_add_test(tc_transaction_api, test_007Transfer_0003BoatPlatonParseRpcResponseResultFail); 
+    tcase_add_test(tc_transaction_api, test_007Transfer_0004TransferSuccessWithoutSync); 
+
+    tcase_add_test(tc_transaction_api, test_008ParseJson_0001ParseJsonFailureNullParam); 
+    tcase_add_test(tc_transaction_api, test_008ParseJson_0002ObtainResultObjectFailure); 
+
     return s_transaction;
 }
