035c3909ccf165ab654a84dddb92be29cdc0d6b9
liuzhenhe <1350928576@qq.com>
2022-10-24 22:07:47 +0800

    [Cita][#1193]update boatcita file

--- sdk/protocol/boatcita/boatcita.c
+++ sdk/protocol/boatcita/boatcita.c
@@ -23,24 +23,26 @@ perform it and wait for its receipt.
 
 #include "boatinternal.h"
 #include "web3intf.h"
-#include "boatethereum.h"
+#include "boatcita.h"
 #include "cJSON.h"
 #include "blockchain.pb-c.h"
 
 
 BOAT_RESULT CitaSendRawtx(BOAT_INOUT BoatCitaTx *tx_ptr)
 {
-    unsigned int chain_id_len;
-        
+
     BCHAR *tx_hash_str;
 
+    BUINT8 sig_parity = 0;
+    BUINT8 sign_result[65] = {0};
     BUINT8 message_digest[32];
     BUINT8 message_digestLen;
-    BUINT8 hash_result[64];
     BCHAR *probuf_hex_str = NULL;
-    BUINT32  packed_trans_unverifiled_length;
-    BUINT32  packed_trans_length;
-    BoatSignatureResult signatureResult;
+    BCHAR *probuf_nonce_str = NULL;
+    BCHAR *probuf_to_str = NULL;
+    BUINT8  *packed_trans_unverifiled_buf = NULL;
+    BUINT32 packed_trans_unverifiled_length;
+    BUINT32 packed_trans_length;
     Param_cita_sendRawTransaction param_cita_sendRawTransaction;
 
     Transaction transaction = TRANSACTION__INIT;
@@ -62,17 +64,54 @@ BOAT_RESULT CitaSendRawtx(BOAT_INOUT BoatCitaTx *tx_ptr)
     *         (See above description for details)                             *
     **************************************************************************/
     // Encode nonce
-    transaction.to    = tx_ptr->rawtx_fields.recipient;
-    transaction.nonce = tx_ptr->rawtx_fields.nonce.field;
+    probuf_to_str = BoatMalloc(BOAT_CITA_ADDRESS_SIZE * 2 + 1);
+    if (probuf_to_str == NULL)
+    {
+        BoatLog(BOAT_LOG_CRITICAL, "Unable to dynamically allocate memory to store protobuf HEX string.");
+        boat_throw(BOAT_ERROR_COMMON_OUT_OF_MEMORY, CitaSendRawtx_cleanup);
+    }
+  
+    UtilityBinToHex(probuf_to_str,
+                tx_ptr->rawtx_fields.recipient, BOAT_CITA_ADDRESS_SIZE,
+                BIN2HEX_LEFTTRIM_UNFMTDATA, BIN2HEX_PREFIX_0x_NO, BOAT_FALSE);
+
+      // Encode nonce
+    probuf_nonce_str = BoatMalloc(16 * 2 + 1);
+    if (probuf_nonce_str == NULL)
+    {
+        BoatLog(BOAT_LOG_CRITICAL, "Unable to dynamically allocate memory to store protobuf HEX string.");
+        boat_throw(BOAT_ERROR_COMMON_OUT_OF_MEMORY, CitaSendRawtx_cleanup);
+    }
+    UtilityBinToHex(probuf_nonce_str,
+                (char*)tx_ptr->rawtx_fields.nonce.field, 16,
+                BIN2HEX_LEFTTRIM_UNFMTDATA, BIN2HEX_PREFIX_0x_NO, BOAT_FALSE);
+    
+    transaction.nonce = probuf_nonce_str;
     transaction.quota = tx_ptr->rawtx_fields.quota;
-    transaction.valid_until_block = tx_ptr->rawtx_fields.valid_until_block
-    transaction.version = tx_ptr->rawtx_fields.version
+    transaction.valid_until_block = tx_ptr->rawtx_fields.valid_until_block;
 
-    transaction.to_v1.data = tx_ptr->rawtx_fields.to_v1.field;
-    transaction.to_v1.len  = tx_ptr->rawtx_fields.to_v1.field_len;
+    transaction.value.data = "0";
+    transaction.value.len = 32;
+  //  transaction.version = tx_ptr->rawtx_fields.version;
+    transaction.version = 2;
 
-    transaction.chain_id_v1.data = tx_ptr->rawtx_fields.chain_id_v1.field;
-    transaction.chain_id_v1.len  = tx_ptr->rawtx_fields.chain_id_v1.field_len;
+    transaction.to_v1.data = probuf_to_str;
+    transaction.to_v1.len  = BOAT_CITA_ADDRESS_SIZE;
+
+    BoatLog_hexdump(BOAT_LOG_VERBOSE, "Transaction Message(to_v1    )", 
+                    transaction.to_v1.data, transaction.to_v1.len);
+
+    BUINT8 chaid_dat[32] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x0, 0x00, 0x00, 0x0, 0x00, 0x00, 0x0, 0x00, 0x00, 0x0, 0x00, 0x00, 0x0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0, 0x00, 0x00, 0x0, 0x00, 0x00, 0x0, 0x00, 0x01};
+                    
+
+
+    transaction.chain_id_v1.data = chaid_dat;
+   // transaction.chain_id_v1.data = tx_ptr->rawtx_fields.chain_id_v1.field;
+  //  transaction.chain_id_v1.len  = tx_ptr->rawtx_fields.chain_id_v1.field_len;
+    transaction.chain_id_v1.len  = 32;
+
+    BoatLog_hexdump(BOAT_LOG_VERBOSE, "Transaction Message(chain_v1   )", 
+                    transaction.chain_id_v1.data, transaction.chain_id_v1.len);
 
     packed_trans_length = transaction__get_packed_size(&transaction);
     hash_data.field_ptr = BoatMalloc(packed_trans_length);
@@ -88,31 +127,66 @@ BOAT_RESULT CitaSendRawtx(BOAT_INOUT BoatCitaTx *tx_ptr)
     /**************************************************************************
     * STEP 2: Calculate SHA3 hash of message                                  *
     **************************************************************************/
-
      // Hash the message
-    result = BoatHash(BOAT_HASH_SHA256,hash_data.field_ptr, 
-                       hash_data.field_len, hash_result, NULL, NULL);
+
+    result = BoatHash(BOAT_HASH_KECCAK256,hash_data.field_ptr, 
+                       hash_data.field_len, message_digest, &message_digestLen, NULL);
 
     if (result != BOAT_SUCCESS) 
     {
         BoatLog(BOAT_LOG_CRITICAL, "Fail to exec BoatHash.");
         boat_throw(result, CitaSendRawtx_cleanup);
+
     }
 
      /**************************************************************************
     * STEP 3: Sign the transaction                                            *
     **************************************************************************/
-    result = BoatSignature(tx_ptr->wallet_ptr->user_cert_prikey_info.prikeyCtx, hash_result, sizeof(hash), &signatureResult, NULL);
 
-    if (result != BOAT_SUCCESS) 
+    BoatSignatureResult signatureResultTmp;
+    result = BoatSignature(tx_ptr->wallet_ptr->account_info.prikeyCtx, 
+                           message_digest, message_digestLen, &signatureResultTmp, NULL);
+    if (result != BOAT_SUCCESS)
     {
-        BoatLog(BOAT_LOG_CRITICAL, "Fail to exec BoatSignature.");
+        BoatLog(BOAT_LOG_CRITICAL, "Fail to BoatSignature signature.");
         boat_throw(BOAT_ERROR_COMMON_GEN_SIGN_FAIL, CitaSendRawtx_cleanup);
     }
 
-    if (!signatureResult.pkcs_format_used) 
+    // assign signature value
+    if (signatureResultTmp.signPrefix_used)
     {
-        BoatLog(BOAT_LOG_CRITICAL, "Fail to find expect signature.");
+        sig_parity = signatureResultTmp.signPrefix;
+    }
+    printf("biatcit v = %d\n", signatureResultTmp.signPrefix);
+
+    // Trim r
+    BUINT8 trimed_r[32] = {0};
+    BUINT8 trimed_r_len = UtilityTrimBin(trimed_r, &signatureResultTmp.native_sign[0],
+                                                    32, TRIMBIN_LEFTTRIM, BOAT_TRUE);
+    memcpy(sign_result, trimed_r, trimed_r_len);
+
+    printf("luussssssssssssssss = %d\n", trimed_r_len);
+    // Trim s
+    BUINT8 trimed_s[32] = {0};
+    BUINT8 trimed_s_len = UtilityTrimBin(trimed_s, &signatureResultTmp.native_sign[32],
+                                                    32, TRIMBIN_LEFTTRIM, BOAT_TRUE);
+
+    printf("luussssssssssssssss = %d\n", trimed_s_len);
+    memcpy(&sign_result[32], trimed_s, trimed_s_len);
+    
+    /**************************************************************************
+    * STEP 4: Encode full RAW transaction with updated v/r/s                  *
+    **************************************************************************/
+    // Re-encode v
+    BUINT8 v = sig_parity;   
+    sign_result[64] = v;
+
+       BoatLog_hexdump(BOAT_LOG_VERBOSE, "Transaction Message(sign111    )", 
+                    sign_result, 65);
+    
+    if (result != BOAT_SUCCESS) 
+    {
+        BoatLog(BOAT_LOG_CRITICAL, "Fail to exec BoatSignature.");
         boat_throw(BOAT_ERROR_COMMON_GEN_SIGN_FAIL, CitaSendRawtx_cleanup);
     }
 
@@ -120,27 +194,47 @@ BOAT_RESULT CitaSendRawtx(BOAT_INOUT BoatCitaTx *tx_ptr)
     * SETP 4: pack the envelope                                               *
     **************************************************************************/
     unverified_transaction.transaction    = &transaction;
-    unverified_transaction.signature.data = signatureResult.pkcs_sign;
-    unverified_transaction.signature.len  = signatureResult.pkcs_sign_length;
+    unverified_transaction.signature.data = sign_result;
+    unverified_transaction.signature.len  = 65;
     unverified_transaction.crypto         = 0;
     packed_trans_unverifiled_length       = unverified_transaction__get_packed_size(&unverified_transaction);
 
-    probuf_hex_str = BoatMalloc(packed_trans_unverifiled_length);
-    if (probuf_hex_str == NULL)
+    packed_trans_unverifiled_buf = BoatMalloc(packed_trans_unverifiled_length);
+    if (packed_trans_unverifiled_buf == NULL)
     {
         BoatLog(BOAT_LOG_NORMAL, "Failed to allocate memory.");
         return BOAT_ERROR_COMMON_OUT_OF_MEMORY;
     }
-    unverified_transaction__pack(&unverified_transaction, probuf_hex_str);
+    unverified_transaction__pack(&unverified_transaction, packed_trans_unverifiled_buf);
+
+    // Allocate memory for RLP stream HEX string
+    // It's a storage for HEX string converted from RLP stream binary. The
+    // HEX string is used as input for web3. It's in a form of "0x1234ABCD".
+    // Where *2 for binary to HEX conversion, +2 for "0x" prefix, + 1 for null terminator.
+    probuf_hex_str = BoatMalloc(packed_trans_unverifiled_length * 2 + 2 + 1);
+    if (probuf_hex_str == NULL)
+    {
+        BoatLog(BOAT_LOG_CRITICAL, "Unable to dynamically allocate memory to store protobuf HEX string.");
+        boat_throw(BOAT_ERROR_COMMON_OUT_OF_MEMORY, CitaSendRawtx_cleanup);
+    }
+    
+    UtilityBinToHex(probuf_hex_str,
+                packed_trans_unverifiled_buf, packed_trans_unverifiled_length,
+                BIN2HEX_LEFTTRIM_UNFMTDATA, BIN2HEX_PREFIX_0x_YES, BOAT_FALSE);
+
+    printf("liuzhenhe = %s\n", probuf_hex_str);
 
 
+    /* print cita transaction message */
+    BoatLog_hexdump(BOAT_LOG_VERBOSE, "Transaction Message(sign    )", 
+                    unverified_transaction.signature.data, unverified_transaction.signature.len);
     /* print cita transaction message */
     BoatLog_hexdump(BOAT_LOG_VERBOSE, "Transaction Message(Nonce    )", 
                     tx_ptr->rawtx_fields.nonce.field, tx_ptr->rawtx_fields.nonce.field_len);
     BoatLog_hexdump(BOAT_LOG_VERBOSE, "Transaction Message(Sender   )", 
-                    tx_ptr->wallet_ptr->account_info.address, BOAT_FISCOBCOS_ADDRESS_SIZE);
+                    tx_ptr->wallet_ptr->account_info.address, BOAT_CITA_ADDRESS_SIZE);
     BoatLog_hexdump(BOAT_LOG_VERBOSE, "Transaction Message(Recipient)", 
-                    tx_ptr->rawtx_fields.recipient, BOAT_FISCOBCOS_ADDRESS_SIZE);
+                    tx_ptr->rawtx_fields.recipient, BOAT_CITA_ADDRESS_SIZE);
     BoatLog_hexdump(BOAT_LOG_VERBOSE, "Transaction Message(Value    )", 
                     tx_ptr->rawtx_fields.value.field, tx_ptr->rawtx_fields.value.field_len);
     BoatLog_hexdump(BOAT_LOG_VERBOSE, "Transaction Message(Data     )", 
@@ -156,12 +250,14 @@ BOAT_RESULT CitaSendRawtx(BOAT_INOUT BoatCitaTx *tx_ptr)
         boat_throw(result, CitaSendRawtx_cleanup);
     }
 
-    result = BoatCitaParseRpcResponseStringResult(tx_hash_str,
+    // result = BoatCitaParseRpcResponseStringResult(tx_hash_str,
+    //                                                    &tx_ptr->wallet_ptr->web3intf_context_ptr->web3_result_string_buf);
+    result = BoatCitaParseRpcResponseResult(tx_hash_str, "hash",
                                                        &tx_ptr->wallet_ptr->web3intf_context_ptr->web3_result_string_buf);
     if (result != BOAT_SUCCESS)
     {
         BoatLog(BOAT_LOG_NORMAL, "Fail to parse RPC response.");
-        boat_throw(result, FiscobcosSendRawtx_cleanup);
+        boat_throw(result, CitaSendRawtx_cleanup);
     }                                        
 
     tx_ptr->tx_hash.field_len = UtilityHexToBin(tx_ptr->tx_hash.field, 32, 
@@ -171,7 +267,7 @@ BOAT_RESULT CitaSendRawtx(BOAT_INOUT BoatCitaTx *tx_ptr)
     result = BOAT_SUCCESS;
 
     // Clean Up
-    boat_catch(FiscobcosSendRawtx_cleanup)
+    boat_catch(CitaSendRawtx_cleanup)
     {
         BoatLog(BOAT_LOG_CRITICAL, "Exception: %d", boat_exception);
         result = boat_exception;
@@ -192,5 +288,134 @@ BOAT_RESULT CitaSendRawtx(BOAT_INOUT BoatCitaTx *tx_ptr)
 }
 
 
-#endif /* end of PROTOCOL_USE_CITA */
+// BOAT_RESULT cita_parse_json_result(const BCHAR *json_string, 
+//                                   const BCHAR *child_name, 
+//                                   BoatFieldVariable *result_out)
+// {
+//     cJSON  *cjson_string_ptr     = NULL;
+//     cJSON  *cjson_result_ptr     = NULL;
+//     cJSON  *cjson_child_name_ptr = NULL;
+//     BCHAR  *parse_result_str     = NULL;
+//     BUINT32 parse_result_str_len;
+//     const char *cjson_error_ptr;
+    
+//     BOAT_RESULT result = BOAT_SUCCESS;
+//     boat_try_declare;
+    
+//     if ((json_string == NULL) || (child_name == NULL) || (result_out == NULL))
+//     {
+//         BoatLog(BOAT_LOG_CRITICAL, "parameter should not be NULL.");
+//         return BOAT_ERROR_COMMON_INVALID_ARGUMENT;
+//     }
+    
+//     // Convert string to cJSON
+//     cjson_string_ptr = cJSON_Parse(json_string);
+//     if (cjson_string_ptr == NULL)
+//     {
+//         cjson_error_ptr = cJSON_GetErrorPtr();
+//         if (cjson_error_ptr != NULL)
+//         {
+//             BoatLog(BOAT_LOG_NORMAL, "Parsing RESPONSE as JSON fails before: %s.", cjson_error_ptr);
+//         }
+//         boat_throw(BOAT_ERROR_WEB3_JSON_PARSE_FAIL, eth_parse_json_result_cleanup);
+//     }
+    
+//     // Obtain result object
+//     cjson_result_ptr = cJSON_GetObjectItemCaseSensitive(cjson_string_ptr, "result");
+//     if (cjson_result_ptr == NULL)
+//     {
+//         BoatLog(BOAT_LOG_NORMAL, "Cannot find \"result\" item in RESPONSE.");
+//         boat_throw(BOAT_ERROR_WEB3_JSON_GETOBJ_FAIL, eth_parse_json_result_cleanup);
+//     }
+
+//     if (cJSON_IsObject(cjson_result_ptr))
+//     {
+//         // the "result" object is json item
+//         cjson_child_name_ptr = cJSON_GetObjectItemCaseSensitive(cjson_result_ptr, child_name);
+//         if (cjson_child_name_ptr == NULL)
+//         {
+//             BoatLog(BOAT_LOG_NORMAL, "Cannot find \"%s\" item in RESPONSE.", child_name);
+//             boat_throw(BOAT_ERROR_WEB3_JSON_GETOBJ_FAIL, eth_parse_json_result_cleanup);
+//         }
+    
+//         //parse child_name object
+//         if (cJSON_IsString(cjson_child_name_ptr))
+//         {
+//             parse_result_str = cJSON_GetStringValue(cjson_child_name_ptr);
+//             if (parse_result_str != NULL)
+//             {
+//                 BoatLog(BOAT_LOG_VERBOSE, "result = %s", parse_result_str);
+
+//                 parse_result_str_len = strlen(parse_result_str);
+
+//                 while(parse_result_str_len >= result_out->field_len)
+//                 {
+//                     BoatLog(BOAT_LOG_VERBOSE, "Expand result_out memory...");
+//                     result = BoatFieldVariable_malloc_size_expand(result_out, WEB3_STRING_BUF_STEP_SIZE);
+//                     if (result != BOAT_SUCCESS)
+//                     {
+//                         BoatLog(BOAT_LOG_CRITICAL, "Failed to excute BoatFieldVariable_malloc_size_expand.");
+//                         boat_throw(BOAT_ERROR_COMMON_OUT_OF_MEMORY, eth_parse_json_result_cleanup);
+//                     }
+//                 }
+//                 strcpy((BCHAR*)result_out->field_ptr, parse_result_str);
+//             }
+//         }
+//         else 
+//         {
+//              strcpy((BCHAR*)result_out->field_ptr, "null");
+//         }
+//     }
+//     else if (cJSON_IsString(cjson_result_ptr))
+//     {
+//         parse_result_str = cJSON_GetStringValue(cjson_result_ptr);
+        
+//         if (parse_result_str != NULL)
+//         {
+//             BoatLog(BOAT_LOG_VERBOSE, "result = %s", parse_result_str);
+
+//             parse_result_str_len = strlen(parse_result_str);
+//             while(parse_result_str_len >= result_out->field_len)
+//             {
+//                 BoatLog(BOAT_LOG_VERBOSE, "Expand result_out memory...");
+//                 result = BoatFieldVariable_malloc_size_expand(result_out, WEB3_STRING_BUF_STEP_SIZE);
+//                 if (result != BOAT_SUCCESS)
+//                 {
+//                     BoatLog(BOAT_LOG_CRITICAL, "Failed to excute BoatFieldVariable_malloc_size_expand.");
+//                     boat_throw(BOAT_ERROR_COMMON_OUT_OF_MEMORY, eth_parse_json_result_cleanup);
+//                 }
+//             }
+//             strcpy((BCHAR*)result_out->field_ptr, parse_result_str);
+//         }
+//     }
+//     else if (cJSON_IsNull(cjson_result_ptr))//cjson_result_ptr:null
+//     {
+//         BoatLog(BOAT_LOG_VERBOSE, "Result is NULL.");
+//         boat_throw(BOAT_ERROR_JSON_OBJ_IS_NULL, eth_parse_json_result_cleanup);
+//     }
+//     else
+//     {
+//         BoatLog(BOAT_LOG_CRITICAL, "Un-expect object type.");
+//         boat_throw(BOAT_ERROR_WEB3_JSON_PARSE_FAIL, eth_parse_json_result_cleanup);
+//     }
+//     if (cjson_string_ptr != NULL)
+//     {
+//         cJSON_Delete(cjson_string_ptr);
+//     }
+    
+//     // Exceptional Clean Up
+//     boat_catch(eth_parse_json_result_cleanup)
+//     {
+//         BoatLog(BOAT_LOG_NORMAL, "Exception: %d", boat_exception);
+
+//         if (cjson_string_ptr != NULL)
+//         {
+//             cJSON_Delete(cjson_string_ptr);
+//         }
+
+//         result = boat_exception;
+//     }
+    
+//     return result;
+// }                        
 
