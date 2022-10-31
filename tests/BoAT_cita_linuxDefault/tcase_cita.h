78b344435949afcc8097bea1cf0f52f0a663257b
liuzhenhe <1350928576@qq.com>
2022-10-31 11:10:28 +0800

    [Cita][#1193]add cita entry function

---
+++ tests/BoAT_cita_linuxDefault/tcase_cita.h
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * Copyright (C) 2018-2021 aitos.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+#ifndef __TCASE_CITA_H__
+#define __TCASE_CITA_H__
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "check.h"
+#include "boatlog.h"
+#include "boattypes.h"
+#include "boatwallet.h"
+#include "protocolapi/api_cita.h"
+
+#define TEST_KEY_TYPE BOAT_WALLET_PRIKEY_FORMAT_NATIVE
+
+extern BOAT_RESULT citaWalletPrepare(void);
+
+extern char cita_private_key_buf[1024];
+extern BUINT8 binFormatKey[32];
+extern BoatCitaWallet *g_cita_wallet_ptr;
+#endif
