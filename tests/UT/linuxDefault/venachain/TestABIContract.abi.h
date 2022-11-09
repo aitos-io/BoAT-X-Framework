8ff3ab8335c56b1ef3e01fa0f0c12f8f40b82d23
zzqAlex <18621758039@163.com>
2022-11-09 11:24:31 +0800

    test[venachain]:Add test contract head file

---
+++ tests/UT/linuxDefault/venachain/TestABIContract.abi.h
@@ -0,0 +1,71 @@
+/******************************************************************************
+This file is generated from contract ABI. DO NOT modify it by hand.
+******************************************************************************/
+
+
+// Generated C function interface from smart contract ABI
+
+#include "boatiotsdk.h"
+
+#ifndef SOLIDITY_TYPE_MAPPING 
+#define SOLIDITY_TYPE_MAPPING 
+
+// Types specially defined for mapping from Solidity
+
+    typedef BUINT8 Bbytes1[1];
+    typedef BUINT8 Bbytes2[2];
+    typedef BUINT8 Bbytes3[3];
+    typedef BUINT8 Bbytes4[4];
+    typedef BUINT8 Bbytes5[5];
+    typedef BUINT8 Bbytes6[6];
+    typedef BUINT8 Bbytes7[7];
+    typedef BUINT8 Bbytes8[8];
+
+    typedef BUINT8 Bbytes9[9];
+    typedef BUINT8 Bbytes10[10];
+    typedef BUINT8 Bbytes11[11];
+    typedef BUINT8 Bbytes12[12];
+    typedef BUINT8 Bbytes13[13];
+    typedef BUINT8 Bbytes14[14];
+    typedef BUINT8 Bbytes15[15];
+    typedef BUINT8 Bbytes16[16];
+
+    typedef BUINT8 Bbytes17[17];
+    typedef BUINT8 Bbytes18[18];
+    typedef BUINT8 Bbytes19[19];
+    typedef BUINT8 Bbytes20[20];
+    typedef BUINT8 Bbytes21[21];
+    typedef BUINT8 Bbytes22[22];
+    typedef BUINT8 Bbytes23[23];
+    typedef BUINT8 Bbytes24[24];
+
+    typedef BUINT8 Bbytes25[25];
+    typedef BUINT8 Bbytes26[26];
+    typedef BUINT8 Bbytes27[27];
+    typedef BUINT8 Bbytes28[28];
+    typedef BUINT8 Bbytes29[29];
+    typedef BUINT8 Bbytes30[30];
+    typedef BUINT8 Bbytes31[31];
+    typedef BUINT8 Bbytes32[32];
+
+    typedef Bbytes16 BUINT128;
+    typedef Bbytes16 BSINT128;
+    typedef Bbytes32 BUINT256;
+    typedef Bbytes32 BSINT256;
+
+#endif 
+BCHAR *TestABIContract_abi_count(BoatVenachainTx *tx_ptr);
+BCHAR *TestABIContract_abi_getAddress(BoatVenachainTx *tx_ptr);
+BCHAR *TestABIContract_abi_getBool(BoatVenachainTx *tx_ptr);
+BCHAR *TestABIContract_abi_getByte(BoatVenachainTx *tx_ptr);
+BCHAR *TestABIContract_abi_getCountTimes(BoatVenachainTx *tx_ptr);
+BCHAR *TestABIContract_abi_getIntArray(BoatVenachainTx *tx_ptr);
+BCHAR *TestABIContract_abi_getString(BoatVenachainTx *tx_ptr);
+BCHAR *TestABIContract_abi_getUint32(BoatVenachainTx *tx_ptr);
+BCHAR *TestABIContract_abi_organizer(BoatVenachainTx *tx_ptr);
+BCHAR *TestABIContract_abi_setAddress(BoatVenachainTx *tx_ptr, BoatAddress input);
+BCHAR *TestABIContract_abi_setBool(BoatVenachainTx *tx_ptr, BUINT8 input);
+BCHAR *TestABIContract_abi_setByte(BoatVenachainTx *tx_ptr, Bbytes32 input);
+BCHAR *TestABIContract_abi_setIntArray(BoatVenachainTx *tx_ptr, BSINT32 *input, BUINT32 inputLen);
+BCHAR *TestABIContract_abi_setString(BoatVenachainTx *tx_ptr, BCHAR*input);
+BCHAR *TestABIContract_abi_setUint32(BoatVenachainTx *tx_ptr, BUINT32 input);
