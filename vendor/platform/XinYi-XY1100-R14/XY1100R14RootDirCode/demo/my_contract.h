7c67c095a4f6e713d50fb0bc6643d13677e57ae9
zzqAlex <18621758039@163.com>
2022-12-21 15:42:48 +0800

    feat[XY1100-R14]:Add XY1100-R14 support

---
+++ vendor/platform/XinYi-XY1100-R14/XY1100R14RootDirCode/demo/my_contract.h
@@ -0,0 +1,12 @@
+/******************************************************************************
+This file is generated from contract ABI. DO NOT modify it by hand.
+******************************************************************************/
+
+
+// Generated C function interface from smart contract ABI
+
+#include "boatconfig.h"
+#include "boatiotsdk.h"
+
+BCHAR * my_contract_cpp_abi_setName(BoatPlatoneTx *tx_ptr, BCHAR* msg);
+BCHAR * my_contract_cpp_abi_getName(BoatPlatoneTx *tx_ptr);
