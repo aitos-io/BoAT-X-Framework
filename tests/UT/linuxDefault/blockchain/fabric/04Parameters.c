d9e49acf7d4c383dc6b077d68b717bd8a1f1cc93
liuzhenhe <1350928576@qq.com>
2022-12-26 14:39:38 +0800

    [fabric][tests]add parameters test case

--- tests/UT/linuxDefault/blockchain/fabric/04Parameters.c
+++ tests/UT/linuxDefault/blockchain/fabric/04Parameters.c
@@ -68,6 +68,7 @@ START_TEST(test_004ParmsInit_0002TxInit_Failure_tx_ptr_NULL)
 
     rtnVal = BoatHlfabricTxInit(NULL, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
     ck_assert_int_eq(rtnVal, BOAT_ERROR_COMMON_INVALID_ARGUMENT);
+    // BoatHlfabricTxDeInit(&tx_ptr);
     BoatIotSdkDeInit();
 }
 END_TEST
@@ -229,6 +230,90 @@ START_TEST(test_004ParmsInit_0011TxSetArgs_argsMoreThanLimit)
 }
 END_TEST
 
+START_TEST(test_004ParmsInit_0012TxSetArgs_Success_argsReachMaxLimit) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricTx tx_ptr;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    g_fabric_wallet_ptr = fabric_get_wallet_ptr();
+
+    rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+   
+    long int timesec = 0;
+    time(&timesec);
+    rtnVal = BoatHlfabricTxSetTimestamp(&tx_ptr, timesec, 0);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricTxSetArgs(&tx_ptr, "invoke", "a", "b", "10", "c","d","e","f","c","d", NULL);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    BoatHlfabricTxDeInit(&tx_ptr);
+    BoatIotSdkDeInit();
+}
+END_TEST
+
+START_TEST(test_004ParmsInit_0013TxSetArgs_Failure_argsMoreThanLimitOne) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricTx tx_ptr;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    g_fabric_wallet_ptr = fabric_get_wallet_ptr();
+
+    rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+   
+    long int timesec = 0;
+    time(&timesec);
+    rtnVal = BoatHlfabricTxSetTimestamp(&tx_ptr, timesec, 0);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricTxSetArgs(&tx_ptr, "invoke", "a", "b", "10", "c","d","e","f","c","d","e", NULL);
+    ck_assert_int_eq(rtnVal, BOAT_ERROR_COMMON_OUT_OF_MEMORY);
+    BoatHlfabricTxDeInit(&tx_ptr);
+    BoatIotSdkDeInit();
+}
+END_TEST
+
+START_TEST(test_004ParmsInit_0014TxSetArgs_Success_argsLessThanLimitOne) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricTx tx_ptr;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    g_fabric_wallet_ptr = fabric_get_wallet_ptr();
+
+    rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+   
+    long int timesec = 0;
+    time(&timesec);
+    rtnVal = BoatHlfabricTxSetTimestamp(&tx_ptr, timesec, 0);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricTxSetArgs(&tx_ptr, "invoke", "a", "b", "10", "c","d","e","f","c", NULL);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    BoatHlfabricTxDeInit(&tx_ptr);
+    BoatIotSdkDeInit();
+}
+END_TEST
+
+START_TEST(test_004ParmsInit_0015TxSetArgs_Success_argsNull) 
+{
+    BSINT32 rtnVal;
+    BoatHlfabricTx tx_ptr;
+    BoatHlfabricWallet *g_fabric_wallet_ptr = NULL;
+    g_fabric_wallet_ptr = fabric_get_wallet_ptr();
+
+    rtnVal = BoatHlfabricTxInit(&tx_ptr, g_fabric_wallet_ptr, NULL, "mycc", NULL, "mychannel", "Org1MSP");
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+   
+    long int timesec = 0;
+    time(&timesec);
+    rtnVal = BoatHlfabricTxSetTimestamp(&tx_ptr, timesec, 0);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    rtnVal = BoatHlfabricTxSetArgs(&tx_ptr, NULL);
+    ck_assert_int_eq(rtnVal, BOAT_SUCCESS);
+    BoatHlfabricTxDeInit(&tx_ptr);
+    BoatIotSdkDeInit();
+}
+END_TEST
+
 Suite *make_fabricParamsTest_suite(void)
 {
     /* Create Suite */
@@ -252,6 +337,9 @@ Suite *make_fabricParamsTest_suite(void)
     tcase_add_test(tc_paramsInit_api, test_004ParmsInit_0009TxSetArgs_Success);
     tcase_add_test(tc_paramsInit_api, test_004ParmsInit_0010TxSetArgs_Txptr_NULL);
     tcase_add_test(tc_paramsInit_api, test_004ParmsInit_0011TxSetArgs_argsMoreThanLimit);
-    
+    tcase_add_test(tc_paramsInit_api, test_004ParmsInit_0012TxSetArgs_Success_argsReachMaxLimit);
+    tcase_add_test(tc_paramsInit_api, test_004ParmsInit_0013TxSetArgs_Failure_argsMoreThanLimitOne);
+    tcase_add_test(tc_paramsInit_api, test_004ParmsInit_0014TxSetArgs_Success_argsLessThanLimitOne);    
+    tcase_add_test(tc_paramsInit_api, test_004ParmsInit_0015TxSetArgs_Success_argsNull);    
     return s_paramsInit;
 }
