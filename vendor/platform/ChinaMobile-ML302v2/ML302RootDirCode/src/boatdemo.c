924258a12054dc783c2b6f8d899f2474b8941d7e
wsdhrtjl <carl.ma@aitos.io>
2022-12-29 17:07:03 +0800

    fix: Modify the PlatONE demo

--- vendor/platform/ChinaMobile-ML302v2/ML302RootDirCode/src/boatdemo.c
+++ vendor/platform/ChinaMobile-ML302v2/ML302RootDirCode/src/boatdemo.c
@@ -2,10 +2,14 @@
 #include "stdlib.h"
 #include "string.h"
 
-#include "cm_main.h" 
+#include "api_platone.h"
 
 #include "boatiotsdk.h"
-#include "api_platone.h"
+#include "boatlog.h"
+
+#include "cm_os.h"
+#include "cm_atchart.h"
+#include "cm_net_register.h"
 
 /**
  * macro used to select wallet type:
@@ -43,105 +47,91 @@ const BCHAR *demoUrl = "http://116.236.47.90:7545";
  */
 const BCHAR *demoRecipientAddress = "0xaac9fb1d70ee0d4b5a857a28b9c3b16114518e45";
 
+//#define cm_printf 
 #define PERSIST_WALLET_NAME     "/nvm/platone.cfg"
 BoatPlatoneWallet *g_platone_wallet_ptr;
+BUINT8 keypairIndex = 0;
+BUINT8 networkIndex = 0;
 
-struct   in_addr test_remote_addr;
-uint16_t test_remote_port;
-char test_msg[] = "network test\n";
-char test_rx_buf[100];
-int cm_global_socket_id = -1;
-osThreadId_t cm_socket_TaskHandle = 0;
-unsigned char cm_socket_buf[1024] = {0};
-static void _cm_socket_select_task(void *not_used);
-/*
-网络初始化配置
-*/
-static bool bNetAutoRegNeedReinit = 0; /* 标识自动网络注册需要初始化 */
 
-//自动注网模块需要添加ATCHART模块才能生效
-#ifdef CM_NET_AUTOREG_SUPPORT
-
-#ifndef CM_ATCHART_SUPPORT
-#error "NET_AUTOREG relies on ATCHART"
-#endif
 static int net_ready = 0;
-//网络自注册接口回调函数，通知网络状态变化
-static void network_state_change(cm_network_state_e state)
+static unsigned char virt_at_buffer[2048];
+static osMutexId_t vir_at_mutex;
+static osEventFlagsId_t at_task_flag; 
+
+static const osMutexAttr_t at_Mutex_attr = 
 {
-	cm_printf("[NET_REGISTER]:state:%d\n",state);
-	switch(state)
-	{
-		//自动注网模块初始化
-		case CM_NETWORK_STATE_INIT:
-			cm_printf("[NET_REGISTER]:Module init\n");
-			break;
-		//无SIM卡
-		case CM_NETWORK_STATE_NO_SIM:
-			cm_printf("[NET_REGISTER]:No SIM\n");
-			break;
-		//SIM卡识别成功
-		case CM_NETWORK_STATE_SIM_READY:
-			cm_printf("[NET_REGISTER]:SIM ready\n");
-			break;
-		//正在附着网络，CEREG:0,2
-		case CM_NETWORK_STATE_ATTACHING:
-			cm_printf("[NET_REGISTER]:Attaching\n");
-			break;
-		//网络附着失败，CEREG:0,0
-		case CM_NETWORK_STATE_ATTACH_FAIL:
-			cm_printf("[NET_REGISTER]:Attach fail\n");
-			break;
-		//网络附着成功，CEREG:0,1
-		case CM_NETWORK_STATE_ATTACH_SUCCESS:
-			cm_printf("[NET_REGISTER]:Attach success\n");
-			break;
-		//正在激活PDP
-		case CM_NETWORK_STATE_PDP_ACTIVATING:
-			cm_printf("[NET_REGISTER]:PDP activating\n");
-			break;
-		//PDP激活失败
-		case CM_NETWORK_STATE_PDP_NOT_READY:
-			cm_printf("[NET_REGISTER]:PDP not ready\n");
-			break;
-		//PDP激活成功
-		case CM_NETWORK_STATE_PDP_READY:
-			net_ready = 1;
-			cm_printf("[NET_REGISTER]:PDP ready\n");
-			break;
-		default:
-			cm_printf("[NET_REGISTER]:Module stop\n");
-			
-	}
+    "atThreadMutex",                          // human readable mutex name
+    osMutexRecursive | osMutexPrioInherit,    // attr_bits
+    0,                                        // memory for control block   
+    0U                                        // size for control block
+};
+
+static void cm_sys_at_callback(void *param)
+{
+    osEventFlagsSet(at_task_flag, 0x00000001U);  
 }
+
 /**
- *  \brief 网络自注册模块初始化
+ *  \brief 虚拟AT指令消息通知回调函数
  *  
+ *  \param [in] pcu8NotifyLine URC消息数据接收指针
+ *  \param [in] u32LineLen URC消息长度
+ *  \param [in] eNotifyType URC消息类型
  *  \return 空
  *  
  *  \details More details
  */
-static void cm_test_net_register_init(void)
+static void cm_test_at_notify(const uint8_t* pcu8NotifyLine, uint32_t u32LineLen, cm_atchart_notify_e eNotifyType)
 {
-	//网络配置结构体
-	cm_network_param_t network_param;
-	
-	//填写APN等信息，
-	network_param.cid = 1;
-	network_param.apn = "CMNET";
-	network_param.pdp_type = "IP";
-	network_param.use_auth = 0;
-
-	
-	//初始化
-	cm_net_register_init();	
-	//网络参数配置
-	cm_net_register_config(&network_param);
-	//设置网络状态变化时的回调函数
-	cm_net_register_set_callback(network_state_change);
-	//启动网络自注册模块
-	cm_net_register_start();
+    if (eNotifyType == CM_ATCHART_RSP_EMPTY_LINE)
+        return;
+    cm_net_register_notify(pcu8NotifyLine, u32LineLen, eNotifyType);
 }
+
+/**
+ *  \brief  虚拟AT指令任务
+ *  
+ *  \param [in] 
+ *  \return NULL
+ *  
+ *  \details More details
+ */
+static void virture_at_task(void)
+{
+    while(1)
+    {
+        osEventFlagsWait(at_task_flag, 0x00000001U, osFlagsWaitAny, osWaitForever);
+        memset(virt_at_buffer,0,2048);
+        int virt_at_len = cm_vir_at_get(virt_at_buffer, 2048);
+        virt_at_buffer[virt_at_len] = 0;
+        cm_sys_log("%s",virt_at_buffer);
+        cm_atchart_on_recv(virt_at_buffer,virt_at_len);
+    }   
+}
+
+static void boat_test_vir_at_init(void)
+{
+    osThreadAttr_t virat_task_attr = {0};
+    at_task_flag = osEventFlagsNew(0);
+    if (at_task_flag == 0)
+    {
+        return;
+    }
+    
+    virat_task_attr.name  = "virat_task";
+    virat_task_attr.stack_size = 2048;
+    
+    vir_at_mutex = osMutexNew(&at_Mutex_attr);
+    // osThreadDef(OC_AT_Task, virture_at_task, osPriorityNormal, 0, 2048);
+    osThreadId_t OC_AT_TaskHandle = osThreadNew(virture_at_task,0,&virat_task_attr);
+    
+    cm_vir_at_init(cm_sys_at_callback);
+    cm_atchart_init();
+    cm_atchart_open();
+    cm_atchart_set_notify_cb(cm_test_at_notify);
+}
+
 /**
  *  \brief 等待网络注册成功
  *  
@@ -151,135 +141,101 @@ static void cm_test_net_register_init(void)
  */
 static int cm_test_wait_for_network(void)
 {
-	//等待时间
-	int retry_count = 50*60; 
-	//将成功标志设为0
-	net_ready = 0;
-	//手动触发网络注册模块更新状态，确保获取到最新网络状态
-	cm_net_register_update_status();
-	//等待一段时间，当超过时间，network_state_change回调函数仍没有返回CM_NETWORK_STATE_PDP_READY，说明网络注册可能遇到问题。
-	while((net_ready == 0)&&(retry_count>0))
-	{
-		osDelay(1);
-		retry_count--;
-	}
-	if(net_ready != 0) //网络注册成功
-	{
-		cm_printf("network ready\n");
-		return 0;
-	}
-	else //网络注册尚未成功
-	{
-		cm_printf("network not ready\n");
-		return -1;
-	}
-		
-	
+    //等待时间
+    int retry_count = 50*60; 
+    //将成功标志设为0
+    net_ready = 0;
+    //手动触发网络注册模块更新状态，确保获取到最新网络状态
+    cm_net_register_update_status();
+    //等待一段时间，当超过时间，network_state_change回调函数仍没有返回CM_NETWORK_STATE_PDP_READY，说明网络注册可能遇到问题。
+    while((net_ready == 0)&&(retry_count>0))
+    {
+        osDelay(1);
+        retry_count--;
+    }
+    if(net_ready != 0) //网络注册成功
+    {
+        cm_sys_log("boat network ready\n");
+        return 0;
+    }
+    else //网络注册尚未成功
+    {
+        cm_sys_log("boat network not ready\n");
+        return -1;
+    }
 }
-#endif
-/**
- *  \brief 网络注册配置
- *  
- *  \return 空
- *  
- *  \details More details	
- */
-void boat_cm_test_network_config(void)
+//网络自注册接口回调函数，通知网络状态变化
+static void network_state_change(cm_network_state_e state)
 {
-#ifdef CM_NET_AUTOREG_SUPPORT
-	//采用ATCHART模块实现的网络自注册方式，实现自动注网
-    cm_test_net_register_init();
-	cm_test_wait_for_network();
-#else
-	//旧网络注册方法，直接使用AT虚拟通道发送AT命令进行网络注册，后续版本可能不再支持，在这种情况下不能使能ATCHART模块，否则会造成冲突
-#ifndef CM_ATCHART_SUPPORT
-	 int i;
-     unsigned char *p =NULL;
-
-	//查询SIM卡
-    for(i = 0;i< 100;i++)
+    cm_sys_log("[boat]:state:%d\n",state);
+    switch(state)
     {
-        p = NULL;
-        p = cm_send_atchart("AT+CPIN?\r\n");
-        if(p !=NULL)
-        {
-            if(strstr(p,"READY") )
-            {
-                break;
-                
-            }   
-        }
-    }        
-    if(i>=100)
-    {
-        cm_printf("sim card error\n");
-        cm_test_dump_at(0,0);
-        return;
-    }
-    
-    p =NULL;
-    p = cm_send_atchart("AT+CFUN=1\r\n");
-    if(p !=NULL)
-    {
-        if(strstr(p,"OK") ==NULL)
-        {
-            cm_printf("cfun error\n");
-            return;
+        //自动注网模块初始化
+        case CM_NETWORK_STATE_INIT:
+            cm_sys_log("[boat]:Module init\n");
+            break;
+        //无SIM卡
+        case CM_NETWORK_STATE_NO_SIM:
+            cm_sys_log("[boat]:No SIM\n");
+            break;
+        //SIM卡识别成功
+        case CM_NETWORK_STATE_SIM_READY:
+            cm_sys_log("[boat]:SIM ready\n");
+            break;
+        //正在附着网络，CEREG:0,2
+        case CM_NETWORK_STATE_ATTACHING:
+            cm_sys_log("[boat]:Attaching\n");
+            break;
+        //网络附着失败，CEREG:0,0
+        case CM_NETWORK_STATE_ATTACH_FAIL:
+            cm_sys_log("[boat]:Attach fail\n");
+            break;
+        //网络附着成功，CEREG:0,1
+        case CM_NETWORK_STATE_ATTACH_SUCCESS:
+            cm_sys_log("[boat]:Attach success\n");
+            break;
+        //正在激活PDP
+        case CM_NETWORK_STATE_PDP_ACTIVATING:
+            cm_sys_log("[boat]:PDP activating\n");
+            break;
+        //PDP激活失败
+        case CM_NETWORK_STATE_PDP_NOT_READY:
+            cm_sys_log("[boat]:PDP not ready\n");
+            break;
+        //PDP激活成功
+        case CM_NETWORK_STATE_PDP_READY:
+            net_ready = 1;
+            cm_sys_log("[boat]:PDP ready\n");
+            break;
+        default:
+            cm_sys_log("[boat]:Module stop\n");
             
-        }   
     }
-    
-    //查询CEREG
-    for(i = 0;i< 120;i++)
-    { 
-        p =NULL;
-        p = cm_send_atchart("AT+CEREG?\r\n");
-        if(p !=NULL)
-        {
-            if(strstr(p,"0,1") ||strstr(p,"1,5") ||strstr(p,"0,5") ||strstr(p,"1,1"))
-            {
-                break;       
-            }   
-        }
-        osDelay(50);
-    }     
-    
-    if(i>=120)
-    {
-        cm_printf("cereg error\n");
-        cm_test_dump_at(0,0);
-        return;
-    }
-    
-	//配置APN
-    p =NULL;
-    p = cm_send_atchart("AT+CGDCONT=1,\"IP\",\"CMNET\"\r\n");
-    if(p !=NULL)
-    {
-        if(strstr(p,"OK") ==NULL)
-        {
-            cm_printf("CGDCONT error\n");
-            return;
-        }   
-    }
-    
-	//激活PDP
-    p =NULL;
-    p = cm_send_atchart("AT+CGACT=1,1\r\n");
-    if(p !=NULL)
-    {
-        if(strstr(p,"OK") ==NULL)
-        {
-            cm_printf("cgact error\n");
-            return;
-        }   
-    }
-    cm_printf("network ready\n");
-#else
-	cm_printf("Network Not Config\n");
-#endif
-#endif
 }
+
+static void cm_test_net_register_init(void)
+{
+    //网络配置结构体
+    cm_network_param_t network_param;
+    
+    //填写APN等信息，
+    network_param.cid = 1;
+    network_param.apn = "CMNET";
+    network_param.pdp_type = "IP";
+    network_param.use_auth = 0;
+
+    //初始化
+    cm_net_register_init(); 
+    //网络参数配置
+    cm_net_register_config(&network_param);
+    //设置网络状态变化时的回调函数
+    cm_net_register_set_callback(network_state_change);
+    //启动网络自注册模块
+    cm_net_register_start();
+}
+
+#define USE_PRIKEY_FORMAT_EXTERNAL_INJECTION_NATIVE
+#define USE_ONETIME_WALLET
 
 __BOATSTATIC BOAT_RESULT platone_createKeypair(BCHAR *keypairName)
 {
@@ -461,28 +417,34 @@ void boat_platone_entry(void)
     return 0;
 }
 
+static void boat_test_network_config(void)
+{
+    //采用ATCHART模块实现的网络自注册方式，实现自动注网
+    cm_test_net_register_init();
+    cm_test_wait_for_network();
+}
 
 void cm_main_task(void *p)
 {  
-	boat_test_vir_at_init(); 
+    boat_test_vir_at_init();
     boat_test_network_config();
+    osDelay(10);
     boat_platone_entry();
     while (1)
     {
-        osDelay(5000);
+        cm_sys_log("[boat] app is running\n");
+        osDelay(500);
     }
 }
 
 void ML302_OpenCPU_Entry()
 {
-    cm_test_net_register_init();
-
 	osThreadAttr_t main_task_attr = {0};
 	main_task_attr.name  = "main_task";
 	main_task_attr.stack_size = 8192;
 
 	osDelay(10);
-    OC_Main_TaskHandle = osThreadNew(cm_main_task, 0, &main_task_attr);    
+    osThreadId_t OC_Main_TaskHandle = osThreadNew(cm_main_task, 0, &main_task_attr);    
 }
 
 //SDK中调用，不可删除
@@ -496,13 +458,13 @@ int cm_wdt_config_cb(void)
 int cm_gnss_uc6226_cb(void)
 {   
     #if ((defined CM_HARDWARE_FNLM_SUPPORT) || (defined CM_HARDWARE_KNLM_SUPPORT) || (defined CM_HARDWARE_GNLM_SUPPORT) || (defined CM_HARDWARE_LNLM_SUPPORT))
-        cm_sys_log("[GPS] cm_gnss_uc6226_cb 1\n");
+        cm_sys_log("[boat] cm_gnss_uc6226_cb 1\n");
 #ifdef CM_PROD_VERSION_H3
     cm_uc6226_gnss_reset(1);
 #endif
         return 1;
     #else
-        cm_sys_log("[GPS] cm_gnss_uc6226_cb 0\n");
+        cm_sys_log("[boat] cm_gnss_uc6226_cb 0\n");
         return 0;
     #endif
 }
@@ -510,11 +472,31 @@ int cm_gnss_uc6226_cb(void)
 int cm_gnss_uctd1036_cb(void)
 {
     #if ((defined CM_HARDWARE_ENLM_SUPPORT) || (defined CM_HARDWARE_PNLM_SUPPORT))
-        cm_sys_log("[GPS] cm_gnss_uctd1036_cb 1\n");
+        cm_sys_log("[boat] cm_gnss_uctd1036_cb 1\n");
         return 1;
     #else
-        cm_sys_log("[GPS] cm_gnss_uctd1036_cb 0\n");
+        cm_sys_log("[boat] cm_gnss_uctd1036_cb 0\n");
         return 0;
     #endif
      
+}
+
+/*
+    function used internal, name is unchangeable
+*/
+int cm_sntp_call_cb(int result)
+{
+    if(result == 0)
+    {
+       cm_sys_log("[boat SNTP SYNC CB]failed\n");
+    }
+    else
+    {
+       cm_sys_log("[boat SNTP SYNC CB]success\n");
+    }
+}
+
+void cm_printf (char *str, ...)
+{
+
 }
