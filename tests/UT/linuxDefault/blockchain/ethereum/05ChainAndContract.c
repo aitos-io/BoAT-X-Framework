30121f016b253c1b3a8e8c777c29e6e3bd011131
zzqAlex <18621758039@163.com>
2023-02-01 14:18:49 +0800

    test[eth][#1345]:Modify function name

--- tests/UT/linuxDefault/blockchain/ethereum/05ChainAndContract.c
+++ tests/UT/linuxDefault/blockchain/ethereum/05ChainAndContract.c
@@ -58,7 +58,7 @@ __BOATSTATIC BoatEthWallet *ethereumOnetimeWalletPrepare()
     memset(network_config.node_url_str,0U,BOAT_ETH_NODE_URL_MAX_LEN);
     strncpy(network_config.node_url_str, "http://127.0.0.1:7545", BOAT_ETH_NODE_URL_MAX_LEN - 1);
 
-    network_index = BoatEthNetworkCreate(&network_config,BOAT_STORE_TYPE_RAM);
+    network_index = BoATEthNetworkCreate(&network_config,BOAT_STORE_TYPE_RAM);
     ck_assert_int_eq(network_index, 0);
 
     wallet_p = BoatEthWalletInit(keypair_index, network_index);
