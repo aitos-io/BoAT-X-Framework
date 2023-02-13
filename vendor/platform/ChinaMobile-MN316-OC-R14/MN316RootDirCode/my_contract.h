67adb9016a726eb658f218a48167921cb21a1e9c
zt222 <tao.zheng@aitos.io>
2023-02-13 18:38:39 +0800

    feat(MN316):
    Port BoAT3.0 to MN316 #1374
    BoATE-1024

---
+++ vendor/platform/ChinaMobile-MN316-OC-R14/MN316RootDirCode/my_contract.h
@@ -0,0 +1,21 @@
+/**
+****************************************************************************************
+* @FilePath: my_contract.h
+* @Author: aitos
+* @Date: 2023-02-13 14:27:53
+* @LastEditors:
+* @LastEditTime: 2023-02-13 14:27:54
+* @Descripttion:
+****************************************************************************************
+*/
+/******************************************************************************
+This file is generated from contract ABI. DO NOT modify it by hand.
+******************************************************************************/
+
+// Generated C function interface from smart contract ABI
+
+#include "boatconfig.h"
+#include "boatiotsdk.h"
+
+BCHAR *my_contract_cpp_abi_setName(BoatPlatoneTx *tx_ptr, BCHAR *msg);
+BCHAR *my_contract_cpp_abi_getName(BoatPlatoneTx *tx_ptr);
