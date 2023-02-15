e77e17ab0437b56f1b61b1d4fd398e51c641cb5f
zzqAlex <18621758039@163.com>
2023-02-15 11:26:42 +0800

    test[platon][#1376]:Modify function name

--- tests/UT/linuxDefault/blockchain/platon/101112ChainAndContract.c
+++ tests/UT/linuxDefault/blockchain/platon/101112ChainAndContract.c
@@ -61,7 +61,7 @@ __BOATSTATIC BoatPlatONWallet *platonOnetimeWalletPrepare()
     memset(network_config.node_url_str,0U,BOAT_PLATON_NODE_URL_MAX_LEN);
     strncpy(network_config.node_url_str, TEST_PLATON_NODE_URL, BOAT_PLATON_NODE_URL_MAX_LEN - 1);
 
-    network_index = BoatPlatONNetworkCreate(&network_config,BOAT_STORE_TYPE_RAM);
+    network_index = BoATPlatONNetworkCreate(&network_config,BOAT_STORE_TYPE_RAM);
     ck_assert_int_eq(network_index, 0);
 
     wallet_p = BoatPlatONWalletInit(keypair_index,network_index);
